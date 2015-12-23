436574a318b15842845412d1f44c5d4306a660f9
Juan Hernandez <juan.hernandez@redhat.com>
2015-12-23 17:30:37 +0100

    Add automation scripts
    This patch adds the automation scripts.
    
    The "build-artifacts.sh" scripts creates the tarball, builds the Maven
    artifacts and generates the JSON and XML descriptions of the model. All
    these artifacts are copied to the exported artifacts directory.
    
    The "check-patch.sh" script runs the unit tests and findbugs.
    
    The "check-merged.sh" script does nothing yet.
    
    Change-Id: Iab06efc6a717bad3212113385eca441343d6a203
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

---
+++ automation/check-patch.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -ex
+
+# Create a settings file that uses the our artifactory server as
+# proxy for all repositories:
+settings="$(pwd)/settings.xml"
+cat > "${settings}" <<.
+<settings>
+  <mirrors>
+
+    <mirror>
+      <id>ovirt-artifactory</id>
+      <url>http://artifactory.ovirt.org/artifactory/ovirt-mirror</url>
+      <mirrorOf>*</mirrorOf>
+    </mirror>
+
+    <mirror>
+      <id>maven-central</id>
+      <url>http://repo.maven.apache.org/maven2</url>
+      <mirrorOf>*</mirrorOf>
+    </mirror>
+
+  </mirrors>
+</settings>
+.
+
+# Clean and then create the local repository:
+repository="repository"
+rm -rf "${repository}"
+mkdir -p "${repository}"
+
+# Prepare an alias for the "mvn" command including all
+# the required options:
+mvn="mvn -s ${settings} -Dmaven.repo.local=${repository}"
+
+# There may be several versions of Java installed in the build
+# enviroment, and we need to make sure that Java 8 is used, as
+# it is required by the code generator:
+export JAVA_HOME="/usr/lib/jvm/java-1.8.0"
+
+# Run the unit tests:
+${mvn} test
+
+# Run findbugs:
+${mvn} install -DskipTests
+${mvn} findbugs:findbugs
