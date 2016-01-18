99b93f836f525678df80182553851747533482ab
Juan Hernandez <juan.hernandez@redhat.com>
2016-01-18 16:56:56 +0100

    Document how to create VM from snapshot
    This patch improves the documentation of the method that creates new
    virtual machines, adding an example of how to create a virtual machine
    from a snapshot with the disks in a different storage domain.
    
    Change-Id: Icba49cf645c4ffb0fe475df725e274808406ac21
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- src/main/java/services/VmsService.java
+++ src/main/java/services/VmsService.java
@@ -24,8 +24,165 @@ import types.Vm;
 @Service
 public interface VmsService {
     /**
-     * Creates a new VM and adds it to the database. The VM is created based on the properties of the `vm` parameter.
-     * The `name`, `templateId` and `clusterId` properties are required.
+     * Creates a new virtual machine.
+     *
+     * The virtual machine can be created in different ways:
+     *
+     * - From a template. In this case the identifier or name of the template must be provided. For example, using a
+     *   plain shell script and XML:
+     *
+     * ```bash
+     * #!/bin/sh -ex
+     *
+     * url="https://engine.example.com/ovirt-engine/api"
+     * user="admin@internal"
+     * password="..."
+     * curl \
+     * --verbose \
+     * --cacert /etc/pki/ovirt-engine/ca.pem \
+     * --user "${user}:${password}" \
+     * --request POST \
+     * --header "Content-Type: application/xml" \
+     * --header "Accept: application/xml" \
+     * --data '
+     * <vm>
+     *   <name>myvm</name>
+     *   <template>
+     *     <name>Blank</name>
+     *   </template>
+     *   <cluster>
+     *     <name>mycluster</name>
+     *   </cluster>
+     * </vm>
+     * ' \
+     * "${url}/vms"
+     * ```
+     *
+     * - From a snapshot. In this case the identifier of the snapshot has to be provided. For example, using a plain
+     *   shel script and XML:
+     *
+     * ```bash
+     * #!/bin/sh -ex
+     *
+     * url="https://engine.example.com/ovirt-engine/api"
+     * user="admin@internal"
+     * password="..."
+     * curl \
+     * --verbose \
+     * --cacert /etc/pki/ovirt-engine/ca.pem \
+     * --user "${user}:${password}" \
+     * --request POST \
+     * --header "Content-Type: application/xml" \
+     * --header "Accept: application/xml" \
+     * --data '
+     * <vm>
+     *   <name>myvm</name>
+     *   <snapshots>
+     *     <snapshot id="266742a5-6a65-483c-816d-d2ce49746680"/>
+     *   </snapshots>
+     *   <cluster>
+     *     <name>mycluster</name>
+     *   </cluster>
+     * </vm>
+     * ' \
+     * "${url}/vms"
+     * ```
+     *
+     * When creating a virtual machine from a template or from a snapshot it is usually useful to explicitly indicate
+     * in what storage domain to create the disks for the virtual machine. If the virtual machine is created from
+     * a template then this is achieved passing a set of `disk` elements that indicate the mapping:
+     *
+     * ```xml
+     * <vm>
+     *   ...
+     *   <disks>
+     *     <disk id="8d4bd566-6c86-4592-a4a7-912dbf93c298">
+     *       <storage_domains>
+     *         <storage_domain id="9cb6cb0a-cf1d-41c2-92ca-5a6d665649c9"/>
+     *       </storage_domains>
+     *     </disk>
+     *   </disks>
+     * </vm>
+     * ```
+     *
+     * When the virtual machine is created from a snapshot this set of disks is sligthly different, it uses the
+     * `imageId` attribute instead of `id`.
+     *
+     * ```xml
+     * <vm>
+     *   ...
+     *   <disks>
+     *     <disk>
+     *       <image_id>8d4bd566-6c86-4592-a4a7-912dbf93c298</image_id>
+     *       <storage_domains>
+     *         <storage_domain id="9cb6cb0a-cf1d-41c2-92ca-5a6d665649c9"/>
+     *       </storage_domains>
+     *     </disk>
+     *   </disks>
+     * </vm>
+     * ```
+     *
+     * In all cases the name or identifier of the cluster where the virtual machine will be created is mandatory.
+     *
+     * This is an example of how creating a virtual machine from a snapshot with the disks in a different storage
+     * domain can be done with the Python SDK:
+     *
+     * ```python
+     * # Find the VM:
+     * vm = api.vms.get(name="myvm")
+     *
+     * # Find the snapshot:
+     * snapshot = None
+     * for current in vm.snapshots.list():
+     *   if current.get_description() == 'mysnap':
+     *     snapshot = current
+     *     break
+     *
+     * # Find the identifiers of the disks of the snapshot, as we need them in
+     * # order to explicitly indicate that we want them created in a different storage
+     * # domain:
+     * disk_ids = []
+     * for current in snapshot.disks.list():
+     *   disk_ids.append(current.get_id())
+     *
+     * # Find the storage domain where the disks should be created:
+     * sd = api.storagedomains.get(name="yourdata")
+     *
+     * # Prepare the list of disks for the operation to create the snapshot,
+     * # explicitly indicating for each of them the storage domain where it should be
+     * # created:
+     * disk_list = []
+     * for disk_id in disk_ids:
+     *   disk = params.Disk(
+     *     image_id=disk_id,
+     *     storage_domains=params.StorageDomains(
+     *       storage_domain=[
+     *         params.StorageDomain(
+     *           id=sd.get_id(),
+     *         ),
+     *       ],
+     *     ),
+     *   )
+     *   disk_list.append(disk)
+     *
+     * # Create the VM from the snapshot:
+     * api.vms.add(
+     *   params.VM(
+     *     name="myclone",
+     *     cluster=params.Cluster(name="mycluster"),
+     *     snapshots=params.Snapshots(
+     *       snapshot=[
+     *         params.Snapshot(
+     *           id=snapshot.get_id(),
+     *         ),
+     *       ],
+     *     ),
+     *     disks=params.Disks(
+     *       disk=disk_list,
+     *     ),
+     *   )
+     * )
+     * ```
      */
     interface Add {
         @In @Out Vm vm();
