2bf1c282cee2fac76e3284630e49ff5e2080ef64
Juan Hernandez <juan.hernandez@redhat.com>
2016-03-16 17:00:16 +0100

    Add missing copyright headers
    This patch adds some missing copyright headers.
    
    Change-Id: I1cbdc0ee8455daae590ef6326150520687b558b7
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- src/main/java/types/CpuMode.java
+++ src/main/java/types/CpuMode.java
@@ -13,13 +13,13 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package types;
 
 import org.ovirt.api.metamodel.annotations.Type;
 
 @Type
 public enum CpuMode {
-
     CUSTOM,
     HOST_MODEL,
     HOST_PASSTHROUGH;
