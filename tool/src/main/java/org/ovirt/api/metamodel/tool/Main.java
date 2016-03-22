cdc518f909defbf7c4870effc1f9a6515101b66e
Juan Hernandez <juan.hernandez@redhat.com>
2016-03-22 13:07:04 +0100

    Make main class generic
    Currently the different tools available in the metamodel have each its
    own "main" class that creates the CDI container and runs the tool. These
    "main" classes are almost identical, the only difference is the name of
    the tool class that they use. This makes original "Main" class generic,
    so that it receives the name of the tool as a parameter, this way the
    other copies can be removed.
    
    Change-Id: I0333e6f3772a297632ed77f4b7f54d65d3d8ea17
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- tool/src/main/java/org/ovirt/api/metamodel/tool/Main.java
+++ tool/src/main/java/org/ovirt/api/metamodel/tool/Main.java
@@ -16,19 +16,69 @@ limitations under the License.
 
 package org.ovirt.api.metamodel.tool;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This class is responsible for bootstrapping the CDI container, creating the application entry point and running it
  * with the command line arguments.
  */
 public class Main {
-    public static void main(String[] args) throws Exception {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        // The first argument must be the name of the tool class:
+        if (args.length < 1) {
+            log.error("The first argument must be the fully qualified name of the tool class.");
+            System.exit(1);
+        }
+        String toolName = args[0];
+
+        // The rest of the arguments are passed to the tool:
+        String[] toolArgs = new String[args.length - 1];
+        System.arraycopy(args, 1, toolArgs, 0, toolArgs.length);
+
+        // Load the tool class:
+        ClassLoader toolLoader = Thread.currentThread().getContextClassLoader();
+        Class<?> toolClass = null;
+        try {
+            toolClass = toolLoader.loadClass(toolName);
+        }
+        catch (ClassNotFoundException exception) {
+            log.error("Can't load tool class \"{}\".", toolName, exception);
+            System.exit(1);
+        }
+
+        // Create the CDI container:
         Weld weld = new Weld();
         WeldContainer container = weld.initialize();
-        Tool tool = container.instance().select(Tool.class).get();
-        tool.run(args);
+
+        // Create a CDI bean for the tool class:
+        Object toolBean = container.instance().select(toolClass).get();
+
+        // Find and execute the "run" method of the CDI bean:
+        Method runMethod = null;
+        try {
+            runMethod = toolClass.getMethod("run", String[].class);
+        }
+        catch (NoSuchMethodException exception) {
+            log.error("Can't find the \"run\" method in tool class \"{}\".", toolName, exception);
+            System.exit(1);
+        }
+        try {
+            runMethod.invoke(toolBean, new Object[] { toolArgs });
+        }
+        catch (IllegalAccessException | InvocationTargetException exception) {
+            log.error("Error while executing the \"run\" method of tool class \"{}\".", toolName, exception);
+            System.exit(1);
+        }
+
+        // When the tool finishes, shutdown the CDI container:
         weld.shutdown();
     }
 }
