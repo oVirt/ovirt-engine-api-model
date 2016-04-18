82a4e1e0992c552db96ce6b786dd18708f08777c
Fred Rolland <frolland@redhat.com>
2016-04-18 11:21:48 +0300

    Added "reportStatus" to Storage Get operation
    The "reportStatus" parameter indicates if the status of the LUNs
    in the storage should be checked.
    Checking the status of the LUN is a heavy weight operation and
    this data is not always needed by the user.
    
    This parameter will give the option to skip the status check
    of the LUNs.
    
    The default is 'true' for backward compatibility.
    
    Change-Id: I396b5d937c45f275d53f5fa631f6f38f1c0f887f
    Bug-Url: https://bugzilla.redhat.com/1308350
    Signed-off-by: Fred Rolland <frolland@redhat.com>

--- src/main/java/services/HostStorageService.java
+++ src/main/java/services/HostStorageService.java
@@ -16,6 +16,7 @@ limitations under the License.
 
 package services;
 
+import org.ovirt.api.metamodel.annotations.In;
 import org.ovirt.api.metamodel.annotations.Out;
 import org.ovirt.api.metamodel.annotations.Service;
 import types.HostStorage;
@@ -24,6 +25,59 @@ import types.HostStorage;
 public interface HostStorageService {
     interface List {
         @Out HostStorage[] storages();
+
+        /**
+         * Indicates if the status of the LUNs in the storage should be checked.
+         * Checking the status of the LUN is an heavy weight operation and
+         * this data is not always needed by the user.
+         * This parameter will give the option to not perform the status check of the LUNs.
+         *
+         * The default is `true` for backward compatibility.
+         *
+         * Here an example with the LUN status :
+         *
+         * [source,xml]
+         * ----
+         * <host_storage id="360014051136c20574f743bdbd28177fd">
+         *   <logical_units>
+         *     <logical_unit id="360014051136c20574f743bdbd28177fd">
+         *       <lun_mapping>0</lun_mapping>
+         *       <paths>1</paths>
+         *       <product_id>lun0</product_id>
+         *       <serial>SLIO-ORG_lun0_1136c205-74f7-43bd-bd28-177fd5ce6993</serial>
+         *       <size>10737418240</size>
+         *       <status>used</status>
+         *       <vendor_id>LIO-ORG</vendor_id>
+         *       <volume_group_id>O9Du7I-RahN-ECe1-dZ1w-nh0b-64io-MNzIBZ</volume_group_id>
+         *     </logical_unit>
+         *   </logical_units>
+         *   <type>iscsi</type>
+         *   <host id="8bb5ade5-e988-4000-8b93-dbfc6717fe50"/>
+         * </host_storage>
+         * ----
+         *
+         * Here an example without the LUN status :
+         *
+         * [source,xml]
+         * ----
+         * <host_storage id="360014051136c20574f743bdbd28177fd">
+         *   <logical_units>
+         *     <logical_unit id="360014051136c20574f743bdbd28177fd">
+         *       <lun_mapping>0</lun_mapping>
+         *       <paths>1</paths>
+         *       <product_id>lun0</product_id>
+         *       <serial>SLIO-ORG_lun0_1136c205-74f7-43bd-bd28-177fd5ce6993</serial>
+         *       <size>10737418240</size>
+         *       <vendor_id>LIO-ORG</vendor_id>
+         *       <volume_group_id>O9Du7I-RahN-ECe1-dZ1w-nh0b-64io-MNzIBZ</volume_group_id>
+         *     </logical_unit>
+         *   </logical_units>
+         *   <type>iscsi</type>
+         *   <host id="8bb5ade5-e988-4000-8b93-dbfc6717fe50"/>
+         * </host_storage>
+         * ----
+         */
+        @In Boolean reportStatus();
     }
 
     @Service StorageService storage(String id);
