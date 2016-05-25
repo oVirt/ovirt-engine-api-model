79f9445bcc2608da9fda4219eba797a78e9f80de
Shmuel Melamud <smelamud@redhat.com>
2016-05-25 15:54:44 +0300

    Added stateful field to VmPool.
    Change-Id: Ia9ebda7bec6529e478ee86738535eb8a22b23775
    Signed-off-by: Shmuel Melamud <smelamud@redhat.com>

--- src/main/java/types/VmPool.java
+++ src/main/java/types/VmPool.java
@@ -30,12 +30,21 @@ public interface VmPool extends Identified {
     VmPoolType type();
     Boolean useLatestTemplateVersion();
 
+    /**
+     * Virtual machine pool's stateful flag.
+     *
+     * Virtual machines from a stateful virtual machine pool are always started in stateful mode
+     * (stateless snapshot is not created). The state of the virtual machine is preserved
+     * even when the virtual machine is passed to a different user.
+     */
+    Boolean stateful();
+
     @Link Cluster cluster();
     @Link Template template();
 
     /**
-     * Reference to the instance type on which this pool is based on. It can be
-     * set only on pool creation and can not be edited.
+     * Reference to the instance type on which this pool is based. It can be
+     * set only on pool creation and cannot be edited.
      */
     @Link InstanceType instanceType();
     @Link Vm vm();
