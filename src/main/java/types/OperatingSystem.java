b6a8a00c15cde8f070ef3419a06378d234f77793
Jakub Niedermertl <jniederm@redhat.com>
2016-06-23 17:16:16 +0200

    Host kernel cmdline API
    OperatingSystem atributes `reportedKernelCmdline` and
    `customKernelCmdline` added.
    
    Change-Id: I113df55cbd03a6c96882832bf27ae87e3e68c965
    Bug-Url: https://bugzilla.redhat.com/1341268
    Signed-off-by: Jakub Niedermertl <jniederm@redhat.com>

--- src/main/java/types/OperatingSystem.java
+++ src/main/java/types/OperatingSystem.java
@@ -18,6 +18,9 @@ package types;
 
 import org.ovirt.api.metamodel.annotations.Type;
 
+/**
+ * Information describing the operating system. Used for virtual machines and hosts.
+ */
 @Type
 public interface OperatingSystem {
     String type();
@@ -25,5 +28,30 @@ public interface OperatingSystem {
     String kernel();
     String initrd();
     String cmdline();
+
+    /**
+     * Host kernel command line as reported by a running host.
+     *
+     * Read-only attribute. Attempts to change this attribute are silently ignored.
+     *
+     * NOTE: This attribute is currently only used for hosts.
+     */
+    String reportedKernelCmdline();
+
+    /**
+     * A custom part of the host kernel command line. This will be merged with the existing kernel command line.
+     *
+     * You must re-install and then reboot the host to apply the changes implemented by this attribute.
+     *
+     * Parameters merging: During each host deploy procedure, kernel parameters that were added
+     * in the previous host deploy procedure are removed using
+     * `grubby --update-kernel DEFAULT --remove-args <previous_custom_params>` and the current
+     * kernel command line customization is applied using
+     * `grubby --update-kernel DEFAULT --args <custom_params>. The Engine internally keeps track of the
+     * last applied kernel parameters customization.
+     *
+     * NOTE: This attribute is currently only used for hosts.
+     */
+    String customKernelCmdline();
     Version version();
 }
