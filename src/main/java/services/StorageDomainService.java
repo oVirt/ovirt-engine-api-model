fe46e4a13b8a9822aad42c32e080d59bf5a6ec78
Juan Hernandez <juan.hernandez@redhat.com>
2016-06-23 18:07:17 +0200

    Document the parameters of the operation that removes a storage domain
    This patch improves the documentation of the `host`, `destroy` and
    `format` parameters of the operation that removes a storage domain.
    
    All this information has been kindly provided by Allon Mureinik.
    
    Change-Id: I044fb389849ed0a04d2a2166405bf60aeafb013d
    Bug-Url: https://bugzilla.redhat.com/1349474
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- src/main/java/services/StorageDomainService.java
+++ src/main/java/services/StorageDomainService.java
@@ -89,7 +89,50 @@ public interface StorageDomainService {
         @In Boolean async();
     }
 
+    /**
+     * Removes the storage domain.
+     *
+     * Without any special parameters, the storage domain is detached from the system and removed fro the database. The
+     * storage domain can then be imported to the same or different setup, with all the data on it. If the storage isn't
+     * accessible the operation will fail.
+     *
+     * If the `destroy` parameter is `true` then the operation will always succeed, even if the storage isn't
+     * accessible, the failure is just ignored and the storage domain is removed from the database anyway.
+     *
+     * If the `format` parameter is `true` then the actual storage is formatted, and the metadata is removed from the
+     * LUN or directory, so it can no longer be imported to the same or a different setup.
+     */
     interface Remove {
+        /**
+         * Indicates what host should be used to remove the storage domain.
+         *
+         * This parameter is mandatory, and it can contain the name or the identifier of the host. For example, to use
+         * the host named `myhost` to remove the storage domain with identifer `123` send a request like this:
+         *
+         * [source]
+         * ----
+         * DELETE /ovirt-engine/api/storagedomains/123?host=myhost
+         * ----
+         */
+        @In String host();
+
+        /**
+         * Indicates if the actual storage should be formatted, removing all the metadata from the underlying LUN or
+         * directory.
+         *
+         * This parameter is optional, and the default value is `false`.
+         */
+        @In Boolean format();
+
+        /**
+         * Indicates if the operation should succeed, and the storage domain removed from the database, even if the
+         * storage isn't accessible.
+         *
+         * This parameter is optiona, and the default value is `false`.
+         */
+        // TODO: Consider renaming this to `force`, or `ignore_errors`, as that describes better what it actually means.
+        @In Boolean destroy();
+
         /**
          * Indicates if the remove should be performed asynchronously.
          */
