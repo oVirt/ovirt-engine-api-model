29b590d5d5bde0936676c52d4156c14467357ef8
Juan Hernandez <juan.hernandez@redhat.com>
2016-08-25 14:34:19 +0200

    Improve the documentation of the VM CDROMs services
    This patch removes the "add" and "remove" operations from these services, as
    adding and removing CDROM devices isn't currently supported. In addition it
    improves the documentation of the supported methods, specially the description
    of how to change and eject CDROM disks.
    
    Change-Id: I90fe26cb963e97dfebb97dbcadb2ce6fe5a42f38
    Related-To: https://bugzilla.redhat.com/1369753
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>
    (cherry picked from commit 1f0152f1cf0436425643456c996ea65c485d022d)

--- src/main/java/services/VmCdromService.java
+++ src/main/java/services/VmCdromService.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2015 Red Hat, Inc.
+Copyright (c) 2015-2016 Red Hat, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -22,26 +22,138 @@ import org.ovirt.api.metamodel.annotations.Out;
 import org.ovirt.api.metamodel.annotations.Service;
 import types.Cdrom;
 
+/**
+ * Manages a CDROM device of a virtual machine.
+ *
+ * If there is a disk inserted then the `file` attribute will contain a reference to the ISO image:
+ *
+ * [source,xml]
+ * ----
+ * <cdrom>
+ *   <file id="mycd.iso"/>
+ * </cdrom>
+ * ----
+ *
+ * If there is no disk inserted then the `file` attribute won't be reported:
+ *
+ * [source,xml]
+ * ----
+ * <cdrom>
+ * </cdrom>
+ * ----
+ *
+ * Changing and ejecting the disk is done using always the `update` method, to change the value of the `file` attribute.
+ * For example, to insert or change the disk send a request like this:
+ *
+ * [source]
+ * ----
+ * PUT /ovirt-engine/api/vms/123/00000000-0000-0000-0000-000000000000
+ * ----
+ *
+ * The body should contain the new value for the `file` attribute:
+ *
+ * [source,xml]
+ * ----
+ * <cdrom>
+ *   <file id="mycd.iso"/>
+ * </cdrom>
+ * ----
+ *
+ * The value of the `id` attribute, `mycd.iso` in this example, should correspond to a file available in an attached
+ * ISO storage domain.
+ *
+ * To eject the disk use a `file` with an empty `id`:
+ *
+ * [source,xml]
+ * ----
+ * <cdrom>
+ *   <file id=""/>
+ * </cdrom>
+ * ----
+ *
+ * By default the above operations change permanently the disk that will be visible to the virtual machine after the
+ * next boot, but they don't have any effect on the currently running virtual machine. If you want to change the disk
+ * that is visible to the current running virtual machine, add the `current=true` parameter. For example, to eject the
+ * current disk send a request like this:
+ *
+ * [source]
+ * ----
+ * PUT /ovirt-engine/api/vms/123/00000000-0000-0000-0000-000000000000?current=true
+ * ----
+ *
+ * With a request body like this:
+ *
+ * [source,xml]
+ * ----
+ * <cdrom>
+ *   <file id=""/>
+ * </cdrom>
+ * ----
+ *
+ * IMPORTANT: The changes made with the `current=true` parameter are never persisted, so they won't have any effect
+ * after the virtual machine is rebooted.
+ *
+ * @author Juan Hernandez <juan.hernandez@redhat.com>
+ * @date 25 Aug 2016
+ * @status added
+ */
 @Service
 @Area("Virtualization")
 public interface VmCdromService {
+    /**
+     * Returns the information about this CDROM device.
+     *
+     * @author Juan Hernandez <juan.hernandez@redhat.com>
+     * @date 25 Aug 2016
+     * @status added
+     */
     interface Get {
+        /**
+         * The information about the CDROM device.
+         *
+         * @author Juan Hernandez <juan.hernandez@redhat.com>
+         * @date 25 Aug 2016
+         * @status added
+         */
         @Out Cdrom cdrom();
+
+        /**
+         * Indicates if the operation should return the information for the currently running virtual machine. This
+         * parameter is optional, and the default value is `false`.
+         *
+         * @author Juan Hernandez <juan.hernandez@redhat.com>
+         * @date 25 Aug 2016
+         * @status added
+         */
+        @In Boolean current();
     }
 
+    /**
+     * Updates the information about this CDROM device.
+     *
+     * @author Juan Hernandez <juan.hernandez@redhat.com>
+     * @date 25 Aug 2016
+     * @status added
+     */
     interface Update {
-        @In @Out Cdrom cdorm();
+        /**
+         * The information about the CDROM device.
+         *
+         * @author Juan Hernandez <juan.hernandez@redhat.com>
+         * @date 25 Aug 2016
+         * @status added
+         */
+        @In @Out Cdrom cdrom();
 
         /**
-         * Indicates if the update should be performed asynchronously.
+         * Indicates if the update should apply to the currently running virtual machine, or to the virtual machine
+         * after the next boot. This parameter is optional, and the default value is `false`, which means that by
+         * default the update will have effect only after the next boot.
+         *
+         * @author Juan Hernandez <juan.hernandez@redhat.com>
+         * @date 25 Aug 2016
+         * @status added
          */
-        @In Boolean async();
-    }
-
-    interface Remove {
-        /**
-         * Indicates if the remove should be performed asynchronously.
-         */
-        @In Boolean async();
+        @In Boolean current();
     }
 }
--- src/main/java/services/VmCdromsService.java
+++ src/main/java/services/VmCdromsService.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2015 Red Hat, Inc.
+Copyright (c) 2015-2016 Red Hat, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -22,21 +22,54 @@ import org.ovirt.api.metamodel.annotations.Out;
 import org.ovirt.api.metamodel.annotations.Service;
 import types.Cdrom;
 
+/**
+ * Manages the CDROM devices of a virtual machine.
+ *
+ * Currently virtual machines have exactly one CDROM device. No new devices can be added, and the existing one can't
+ * be removed, thus there are no `add` or `remove` methods. Changing and ejecting CDROM disks is done with the
+ * <<services/vm_cdrom/methods/update, update>> method of the <<services/vm_cdrom, service>> that manages the
+ * CDROM device.
+ *
+ * @author Juan Hernandez <juan.hernandez@redhat.com>
+ * @date 25 Aug 2016
+ * @status added
+ */
 @Service
 @Area("Virtualization")
 public interface VmCdromsService {
-    interface Add {
-        @In @Out Cdrom cdrom();
-    }
-
+    /**
+     * Returns the list of CDROM devices of the virtual machine.
+     *
+     * @author Juan Hernandez <juan.hernandez@redhat.com>
+     * @date 25 Aug 2016
+     * @status added
+     */
     interface List {
+        /**
+         * The list of CDROM devices of the virtual machine.
+         *
+         * @author Juan Hernandez <juan.hernandez@redhat.com>
+         * @date 25 Aug 2016
+         * @status added
+         */
         @Out Cdrom[] cdroms();
 
         /**
          * Sets the maximum number of CDROMs to return. If not specified all the CDROMs are returned.
+         *
+         * @author Juan Hernandez <juan.hernandez@redhat.com>
+         * @date 25 Aug 2016
+         * @status added
          */
         @In Integer max();
     }
 
+    /**
+     * Returns a reference to the service that manages a specific CDROM device.
+     *
+     * @author Juan Hernandez <juan.hernandez@redhat.com>
+     * @date 25 Aug 2016
+     * @status added
+     */
     @Service VmCdromService cdrom(String id);
 }
