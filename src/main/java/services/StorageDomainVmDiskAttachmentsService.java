a980db6605e709c704aa923ce60509bbefb2a0b6
Tal Nisan <tnisan@redhat.com>
2016-09-06 14:21:45 +0300

    Add disk attachments collection to a VM on export domain
    Change-Id: I63a68e9edb4418a9d75701a57413cc719ed4b137
    Bug-Url: https://bugzilla.redhat.com/1371024
    Signed-off-by: Tal Nisan <tnisan@redhat.com>
    (cherry picked from commit b20703607f5ad302d81d7df2abaed2aaea57bdde)

---
+++ src/main/java/services/StorageDomainVmDiskAttachmentsService.java
@@ -0,0 +1,58 @@
+/*
+Copyright (c) 2016 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package services;
+
+import org.ovirt.api.metamodel.annotations.Out;
+import org.ovirt.api.metamodel.annotations.Service;
+
+import annotations.Area;
+import types.DiskAttachment;
+
+/**
+ * Returns the details of a disk attached to a virtual machine in the export domain.
+ *
+ * @author Tal Nisan <tnisan@redhat.com>
+ * @date 6 Sep 2016
+ * @status added
+ * @since 4.0.4
+ */
+@Service
+@Area("Storage")
+public interface StorageDomainVmDiskAttachmentsService {
+
+    /**
+     * List the disks that are attached to the virtual machine.
+     *
+     * @author Tal Nisan <tnisan@redhat.com>
+     * @date 6 Sep 2016
+     * @status added
+     * @since 4.0.4
+     */
+    interface List {
+        @Out DiskAttachment[] attachments();
+    }
+
+    /**
+     * Reference to the service that manages a specific attachment.
+     *
+     * @author Tal Nisan <tnisan@redhat.com>
+     * @date 6 Sep 2016
+     * @status added
+     * @since 4.0.4
+     */
+    @Service StorageDomainVmDiskAttachmentService attachment(String id);
+}
