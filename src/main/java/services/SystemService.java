fe3e4924176590e6e1c5081d5d99f46745aa7ef1
Piotr Kliczewski <piotr.kliczewski@gmail.com>
2016-09-14 10:53:59 +0200

    docs: event and api entry
    This patch adds documentation to:
    - how to remove an event using event service.
    - how to use api entry point and what data is
      returns.
    - how to get a list of events that occurred.
    - how to search events by severity and how to
      paginate the result of a search.
    
    Change-Id: Ibeb962b9872f6628235af5a02b53d381f2aba7ba
    Signed-off-by: Piotr Kliczewski <piotr.kliczewski@gmail.com>

--- src/main/java/services/SystemService.java
+++ src/main/java/services/SystemService.java
@@ -34,9 +34,91 @@ import types.Api;
 @Root
 @Area("Infrastructure")
 public interface SystemService {
+
     /**
      * Returns basic information describing the API, like the product name, the version number and a summary of the
      * number of relevant objects.
+     *
+     * [source]
+     * ----
+     * GET /ovirt-engine/api
+     * ----
+     *
+     * We get following response:
+     *
+     * [source,xml]
+     * ----
+     * <api>
+     *   <link rel="capabilities" href="/api/capabilities"/>
+     *   <link rel="clusters" href="/api/clusters"/>
+     *   <link rel="clusters/search" href="/api/clusters?search={query}"/>
+     *   <link rel="datacenters" href="/api/datacenters"/>
+     *   <link rel="datacenters/search" href="/api/datacenters?search={query}"/>
+     *   <link rel="events" href="/api/events"/>
+     *   <link rel="events/search" href="/api/events?search={query}"/>
+     *   <link rel="hosts" href="/api/hosts"/>
+     *   <link rel="hosts/search" href="/api/hosts?search={query}"/>
+     *   <link rel="networks" href="/api/networks"/>
+     *   <link rel="roles" href="/api/roles"/>
+     *   <link rel="storagedomains" href="/api/storagedomains"/>
+     *   <link rel="storagedomains/search" href="/api/storagedomains?search={query}"/>
+     *   <link rel="tags" href="/api/tags"/>
+     *   <link rel="templates" href="/api/templates"/>
+     *   <link rel="templates/search" href="/api/templates?search={query}"/>
+     *   <link rel="users" href="/api/users"/>
+     *   <link rel="groups" href="/api/groups"/>
+     *   <link rel="domains" href="/api/domains"/>
+     *   <link rel="vmpools" href="/api/vmpools"/>
+     *   <link rel="vmpools/search" href="/api/vmpools?search={query}"/>
+     *   <link rel="vms" href="/api/vms"/>
+     *   <link rel="vms/search" href="/api/vms?search={query}"/>
+     *   <product_info>
+     *     <name>oVirt Engine</name>
+     *     <vendor>ovirt.org</vendor>
+     *     <version>
+     *       <build>4</build>
+     *       <full_version>4.0.4</full_version>
+     *       <major>4</major>
+     *       <minor>0</minor>
+     *       <revision>0</revision>
+     *     </version>
+     *   </product_info>
+     *   <special_objects>
+     *     <blank_template href="/ovirt-engine/api/templates/00000000-0000-0000-0000-000000000000" id="00000000-0000-0000-0000-000000000000"/>
+     *     <root_tag href="/ovirt-engine/api/tags/00000000-0000-0000-0000-000000000000" id="00000000-0000-0000-0000-000000000000"/>
+     *   </special_objects>
+     *   <summary>
+     *     <hosts>
+     *       <active>0</active>
+     *       <total>0</total>
+     *     </hosts>
+     *     <storage_domains>
+     *       <active>0</active>
+     *       <total>1</total>
+     *     </storage_domains>
+     *     <users>
+     *       <active>1</active>
+     *       <total>1</total>
+     *     </users>
+     *     <vms>
+     *       <active>0</active>
+     *       <total>0</total>
+     *     </vms>
+     *   </summary>
+     *   <time>2016-09-14T12:00:48.132+02:00</time>
+     * </api>
+     * ----
+     *
+     * The entry point provides a user with links to the collections in a
+     * virtualization environment. The `rel` attribute of each collection link
+     * provides a reference point for each link.
+     *
+     * The entry point also contains other data such as `product_info`,
+     * `special_objects` and `summary`.
+     *
+     * @author Piotr Kliczewski <pkliczew@redhat.com>
+     * @date 14 Sep 2016
+     * @status added
      */
     interface Get {
         @Out Api api();
