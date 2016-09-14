722e93e224df1aeb164ffc58120cc423f45362d2
Marcin Mirecki <mmirecki@redhat.com>
2016-09-14 13:02:00 +0200

    Add MAC address pool documentation
    Signed-off-by: Marcin Mirecki <mmirecki@redhat.com>
    Change-Id: Id93afe8f2b3e2e6c36bd643be210d26402abb428
    (cherry picked from commit 7e4e041fdb08fd02c68954066d1bed9f42e7ee31)

--- src/main/java/types/MacPool.java
+++ src/main/java/types/MacPool.java
@@ -18,9 +18,57 @@ package types;
 
 import org.ovirt.api.metamodel.annotations.Type;
 
+/**
+ * Represents a MAC address pool.
+ *
+ * Example of a XML representatian of a MAC address pool:
+ *
+ * [source,xml]
+ * ----
+ * <mac_pool href="/ovirt-engine/api/macpools/123" id="123">
+ *   <name>Default</name>
+ *   <description>Default MAC pool</description>
+ *   <allow_duplicates>false</allow_duplicates>
+ *   <default_pool>true</default_pool>
+ *   <ranges>
+ *     <range>
+ *       <from>00:1A:4A:16:01:51</from>
+ *       <to>00:1A:4A:16:01:E6</to>
+ *     </range>
+ *   </ranges>
+ * </mac_pool>
+ * ----
+ *
+ * @author Marcin Mirecki <mmirecki@redhat.com>
+ * @date 14 Sept 2016
+ * @status added
+ */
 @Type
 public interface MacPool extends Identified {
+    /**
+     * Defines whether duplicate MAC addresses are permitted in the pool. If not specified defaults to `false`.
+     *
+     * @author Marcin Mirecki <mmirecki@redhat.com>
+     * @date 22 Sept 2016
+     * @status added
+     */
     Boolean allowDuplicates();
+
+    /**
+     * Defines whether this is the default pool. If not specified defaults to `false`.
+     *
+     * @author Marcin Mirecki <mmirecki@redhat.com>
+     * @date 22 Sept 2016
+     * @status added
+     */
     Boolean defaultPool();
+
+    /**
+     * Defines the range of MAC addresses for the pool. Multiple ranges can be defined.
+     *
+     * @author Marcin Mirecki <mmirecki@redhat.com>
+     * @date 22 Sept 2016
+     * @status added
+     */
     Range[] ranges();
 }
