4738b8e7d6cd9c5193f7d082ff0a4683ce67155f
Tomas Jelinek <tjelinek@redhat.com>
2016-09-14 14:44:23 +0200

    Documented power management elements.
    Change-Id: Ib48867ff3c1056ca4d28d072317f71240bab9791
    Signed-off-by: Tomas Jelinek <tjelinek@redhat.com>
    (cherry picked from commit 81eb9ce0d51e18ecacd89b8f05bb331ca40a6b56)

--- src/main/java/types/PowerManagement.java
+++ src/main/java/types/PowerManagement.java
@@ -20,16 +20,114 @@ import org.ovirt.api.metamodel.annotations.Type;
 
 @Type
 public interface PowerManagement {
+
+    /**
+     * Indicates whether power management configuration is enabled or disabled.
+     *
+     * @author Tomas Jelinek <tjelinek@redhat.com>
+     * @date 14 Sep 2016
+     * @status added
+     */
     Boolean enabled();
+
+    /**
+     * The host name or IP address of the host.
+     *
+     * @author Tomas Jelinek <tjelinek@redhat.com>
+     * @date 14 Sep 2016
+     * @status added
+     */
     String address();
+
+    /**
+     * A valid user name for power management.
+     *
+     * @author Tomas Jelinek <tjelinek@redhat.com>
+     * @date 14 Sep 2016
+     * @status added
+     */
     String username();
+
+    /**
+     * A valid, robust password for power management.
+     *
+     * @author Tomas Jelinek <tjelinek@redhat.com>
+     * @date 14 Sep 2016
+     * @status added
+     */
     String password();
+
+    /**
+     * Fencing options for the selected type= specified with the option name="" and value="" strings.
+     *
+     * @author Tomas Jelinek <tjelinek@redhat.com>
+     * @date 14 Sep 2016
+     * @status added
+     */
     Option[] options();
+
+    /**
+     * Determines the power status of the host.
+     *
+     * @author Tomas Jelinek <tjelinek@redhat.com>
+     * @date 14 Sep 2016
+     * @status added
+     */
     PowerManagementStatus status();
+
+    /**
+     * Determines the power management proxy.
+     *
+     * @author Tomas Jelinek <tjelinek@redhat.com>
+     * @date 14 Sep 2016
+     * @status added
+     */
     PmProxy[] pmProxies();
+
+    /**
+     * Specifies fence agent options when multiple fences are used.
+     *
+     * Use the order sub-element to prioritize the fence agents. Agents are run sequentially according to their
+     * order until the fence action succeeds. When two or more fence agents have the same order,
+     * they are run concurrently. Other sub-elements include type, ip, user, password, and options.
+     *
+     * @author Tomas Jelinek <tjelinek@redhat.com>
+     * @date 14 Sep 2016
+     * @status added
+     */
     Agent[] agents();
+
+    /**
+     * Toggles the automated power control of the host in order to save energy.
+     * When set to true, the host will be automatically powered down if the cluster's load is low,
+     * and powered on again when required. This is set to true when a host is created, unless disabled by the user.
+     *
+     * @author Tomas Jelinek <tjelinek@redhat.com>
+     * @date 14 Sep 2016
+     * @status added
+     */
     Boolean automaticPmEnabled();
+
+    /**
+     * Toggles whether to determine if kdump is running on the host before it is shut down.
+     * When set to `true`, the host will not shut down during a kdump process.
+     * This is set to `true` when a host has power management enabled, unless disabled by the user.
+     *
+     * @author Tomas Jelinek <tjelinek@redhat.com>
+     * @date 14 Sep 2016
+     * @status added
+     */
     Boolean kdumpDetection();
+
+    /**
+     * Fencing device code.
+     *
+     * A list of valid fencing device codes are available in the `capabilities` collection.
+     *
+     * @author Tomas Jelinek <tjelinek@redhat.com>
+     * @date 14 Sep 2016
+     * @status added
+     */
     // TODO: (deprecated) remove 'type' post 4.0
     String type();
 }
