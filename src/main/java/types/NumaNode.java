5e2001cb955f10a5e814663e757cbb4fd928cdc2
Andrej Krejcir <akrejcir@redhat.com>
2016-09-14 14:53:56 +0200

    doc: add host NUMA node XML example
    Change-Id: I4fded84ecdab60e742c0adf2cc30b46e1d6117cd
    Signed-off-by: Andrej Krejcir <akrejcir@redhat.com>

--- src/main/java/types/NumaNode.java
+++ src/main/java/types/NumaNode.java
@@ -19,9 +19,43 @@ package types;
 import org.ovirt.api.metamodel.annotations.Link;
 import org.ovirt.api.metamodel.annotations.Type;
 
+/**
+ * Represents a physical NUMA node.
+ *
+ * Example XML representation:
+ *
+ * [source,xml]
+ * ----
+ * <host_numa_node href="/ovirt-engine/api/hosts/0923f1ea/numanodes/007cf1ab" id="007cf1ab">
+ *   <cpu>
+ *     <cores>
+ *       <core>
+ *         <index>0</index>
+ *       </core>
+ *     </cores>
+ *   </cpu>
+ *   <index>0</index>
+ *   <memory>65536</memory>
+ *   <node_distance>40 20 40 10</node_distance>
+ *   <host href="/ovirt-engine/api/hosts/0923f1ea" id="0923f1ea"/>
+ * </host_numa_node>
+ * ----
+ *
+ * @author Andrej Krejcir <akrejcir@redhat.com>
+ * @date 14 Sep 2016
+ * @status added
+ */
 @Type
 public interface NumaNode extends Identified {
     Integer index();
+
+    /**
+     * Memory of the NUMA node in MB.
+     *
+     * @author Andrej Krejcir <akrejcir@redhat.com>
+     * @date 14 Sep 2016
+     * @status added
+     */
     Integer memory();
     Cpu cpu();
     String nodeDistance();
