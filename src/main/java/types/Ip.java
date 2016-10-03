7d9038991c02ad5860403021b4201a84d32162cb
Juan Hernandez <juan.hernandez@redhat.com>
2016-10-03 12:51:01 +0200

    Make Ip.version optional
    The value of the "Ip.version" attribute can be inferred from the value
    "Ip.address" attribute. This patch documents that behaviour, which will be
    introduced in version 4.0.6 of the engine.
    
    Change-Id: I0ce1d9302d2812176e1cab3aaa31325f3c4033ec
    Related-To: https://bugzilla.redhat.com/1381240
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- src/main/java/types/Ip.java
+++ src/main/java/types/Ip.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2015 Red Hat, Inc.
+Copyright (c) 2015-2016 Red Hat, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,10 +18,75 @@ package types;
 
 import org.ovirt.api.metamodel.annotations.Type;
 
+/**
+ * Represents the IP configuration of a network interface.
+ *
+ * @author Juan Hernandez <juan.hernandez@redhat.com>
+ * @date 3 Oct 2016
+ * @staus added
+ */
 @Type
 public interface Ip {
+    /**
+     * The text representation of the IP address.
+     *
+     * For example, an IPv4 address will be represented as follows:
+     *
+     * [source,xml]
+     * ----
+     * <ip>
+     *   <address>192.168.0.1</address>
+     *   ...
+     * </ip>
+     * ----
+     *
+     * An IPv6 address will be represented as follows:
+     *
+     * [source,xml]
+     * ----
+     * <ip>
+     *   <address>2620:52:0:20f0:4216:7eff:feaa:1b50</address>
+     *   ...
+     * </ip>
+     * ----
+     *
+     * @author Juan Hernandez <juan.hernandez@redhat.com>
+     * @date 3 Oct 2016
+     * @staus added
+     */
     String address();
+
+    /**
+     * The network mask.
+     *
+     * @author Juan Hernandez <juan.hernandez@redhat.com>
+     * @date 3 Oct 2016
+     * @staus added
+     */
     String netmask();
+
+    /**
+     * The address of the default gateway.
+     *
+     * @author Juan Hernandez <juan.hernandez@redhat.com>
+     * @date 3 Oct 2016
+     * @staus added
+     */
     String gateway();
+
+    /**
+     * The version of the IP protocol.
+     *
+     * This attribute is optional, and when it isn't given the value will be inferred from the value of the `address`
+     * attribute. If that value is a valid IPv4 address, then it will be assumed that the value of this attribute is
+     * `v4`. If it is a valid IPv6 address, then it will be assumed that the value of this attribute is `v6`.
+     *
+     * NOTE: This inference of the IP version from the value of the `address` is implemented since version 4.1 of the
+     * engine, before that this attribute was mandatory.
+     *
+     * @author Juan Hernandez <juan.hernandez@redhat.com>
+     * @date 3 Oct 2016
+     * @staus added
+     */
     IpVersion version();
 }
