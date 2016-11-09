6ef69c8cc44c9ecbd516989017940cbfc37c7b38
Jakub Niedermertl <jniederm@redhat.com>
2016-11-09 15:08:57 +0100

    MemoryPolicy#max property added
    Change-Id: Ia3e1f5a2b482f8912d9dc95bc7ad92d2e6ba0441
    Signed-off-by: Jakub Niedermertl <jniederm@redhat.com>
    Related-To: https://bugzilla.redhat.com/1388245

--- src/main/java/types/MemoryPolicy.java
+++ src/main/java/types/MemoryPolicy.java
@@ -18,9 +18,32 @@ package types;
 
 import org.ovirt.api.metamodel.annotations.Type;
 
+/**
+ * Logical grouping of memory related properties of virtual machine-like entities.
+ *
+ * @author Jakub Niedermertl <jniederm@redhat.com>
+ * @date 9 Nov 2016
+ * @status added
+ */
 @Type
 public interface MemoryPolicy {
     Integer guaranteed();
+
+    /**
+     * Maximum virtual machine's memory, in bytes.
+     *
+     * The user provides the value in bytes, and the engine rounds the value down to the nearest lower MiB value.
+     *
+     * For example, if the user enters a value of 1073741825 (1 GiB + 1 byte), then the {engine-name} will truncate
+     * that value to the nearest lower MiB boundary: in this case 1073741824 (1 GiB).
+     *
+     * @author Jakub Niedermertl <jniederm@redhat.com>
+     * @date 9 Nov 2016
+     * @status added
+     * @since 4.1
+     */
+    Integer max();
+
     Boolean ballooning();
     MemoryOverCommit overCommit();
     TransparentHugePages transparentHugePages();
