d1b786e5c016f5b6290dfc30fa413d06ca114367
Byron Gravenorst <bgraveno@redhat.com>
2016-11-15 11:19:46 +1000

    File ImageTransfer.java
    Updated the documentation for the ImageTransfer type.
    
    Change-Id: I6385012fd79ce2186b8feb45a40cfd7dce9e5836
    Signed-off-by: Byron Gravenorst <bgraveno@redhat.com>
    (cherry picked from commit 0ea22527f394041df86b12845bd5ce3f95b782b4)

--- src/main/java/types/ImageTransfer.java
+++ src/main/java/types/ImageTransfer.java
@@ -23,44 +23,48 @@ import org.ovirt.api.metamodel.annotations.Type;
  * This type contains information regarding an image transfer being performed.
  *
  * @author Amit Aviram <aaviram@redhat.com>
- * @date 30 Aug 2016
- * @status added
+ * @author Byron Gravenorst <bgraveno@redhat.com>
+ * @date 15 Nov 2016
+ * @status updated_by_docs
  * @since 4.0.4
  */
 @Type
 public interface ImageTransfer extends Identified {
     /**
-     * The URL of the proxy server which the user should do I/O to. This attribute is
-     * available only if the image transfer entity is in <<types/image_transfer_phase, transferring>>
+     * The URL of the proxy server that the user inputs or outputs to. This attribute is
+     * available only if the image transfer entity is in the <<types/image_transfer_phase, transferring>>
      * phase. See `phase` for details.
      *
      * @author Amit Aviram <aaviram@redhat.com>
-     * @date 30 Aug 2016
-     * @status added
+     * @author Byron Gravenorst <bgraveno@redhat.com>
+     * @date 15 Nov 2016
+     * @status updated_by_docs
      * @since 4.0.4
      */
     String proxyUrl();
 
     /**
      * The signed ticket that should be attached as an `Authentication` header in the
-     * HTTPS request for the proxy server to do I/O to (See `proxy_url` attribute).
+     * HTTPS request for the proxy server to input or output to (See the `proxy_url` attribute).
      *
      * @author Amit Aviram <aaviram@redhat.com>
-     * @date 30 Aug 2016
-     * @status added
+     * @author Byron Gravenorst <bgraveno@redhat.com>
+     * @date 15 Nov 2016
+     * @status updated_by_docs
      * @since 4.0.4
      */
     String signedTicket();
 
     /**
-     * The current phase of the image transfer being made. Each transfer needs a managed
-     * session, which must be opened in order for the user to be able to do I/O to an image.
+     * The current phase of the image transfer in progress. Each transfer needs a managed
+     * session, which must be opened for the user to input or output an image.
      * Please refer to <<services/image_transfer, image transfer>> for further
      * documentation.
      *
      * @author Amit Aviram <aaviram@redhat.com>
-     * @date 30 Aug 2016
-     * @status added
+     * @author Byron Gravenorst <bgraveno@redhat.com>
+     * @date 15 Nov 2016
+     * @status updated_by_docs
      * @since 4.0.4
      */
     ImageTransferPhase phase();
@@ -69,18 +73,20 @@ public interface ImageTransfer extends Identified {
      * The image which is targeted for I/O.
      *
      * @author Amit Aviram <aaviram@redhat.com>
-     * @date 30 Aug 2016
-     * @status added
+     * @author Byron Gravenorst <bgraveno@redhat.com>
+     * @date 15 Nov 2016
+     * @status updated_by_docs
      * @since 4.0.4
      */
     @Link Image image();
 
     /**
-     * The host which will be used to write to the image which is targeted for I/O.
+     * The host which will be used to write to the image which is targeted for input or output.
      *
      * @author Amit Aviram <aaviram@redhat.com>
-     * @date 30 Aug 2016
-     * @status added
+     * @author Byron Gravenorst <bgraveno@redhat.com>
+     * @date 15 Nov 2016
+     * @status updated_by_docs
      * @since 4.0.4
      */
     @Link Host host();
