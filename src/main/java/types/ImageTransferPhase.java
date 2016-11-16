7d084f2ceb97ca52b0f053fc9d905b43fad26d53
Byron Gravenorst <bgraveno@redhat.com>
2016-11-17 09:13:01 +1000

    Document ImageTransferPhase.java
    Updated the documentation for the ImageTransferPhase type.
    
    Change-Id: I1a35f1ed0bf0434edffa9ba35f51e57fef5928e7
    Signed-off-by: Byron Gravenorst <bgraveno@redhat.com>

--- src/main/java/types/ImageTransferPhase.java
+++ src/main/java/types/ImageTransferPhase.java
@@ -22,138 +22,150 @@ import org.ovirt.api.metamodel.annotations.Type;
  * A list of possible phases for an <<types/image_transfer, image transfer>> entity. Each of these values
  * defines a specific point in a transfer flow.
  *
- * Please refer to <<services/image_transfer, image transfer>> for further
- * documentation.
+ * Please refer to <<services/image_transfer, image transfer>> for more
+ * information.
  *
  * @author Amit Aviram <aaviram@redhat.com>
- * @date 30 Aug 2016
- * @status added
+ * @author Byron Gravenorst <bgraveno@redhat.com>
+ * @date 15 Nov 2016
+ * @status updated_by_docs
  * @since 4.0.4
  */
 @Type
 public enum ImageTransferPhase {
     /**
-     * An unknown phase, will only be set in cases of unpredictable errors.
+     * An unknown phase. This will only be set in cases of unpredictable errors.
      *
      * @author Amit Aviram <aaviram@redhat.com>
-     * @date 30 Aug 2016
-     * @status added
+     * @author Byron Gravenorst <bgraveno@redhat.com>
+     * @date 15 Nov 2016
+     * @status updated_by_docs
      * @since 4.0.4
      */
     UNKNOWN,
 
     /**
-     * The initial phase of an added image transfer. It is set as long as the transfer session is establishing.
+     * The initial phase of an image transfer. It is set while the transfer session is establishing.
      * Once the session is established, the phase will be changed to `transferring`
      *
      * @author Amit Aviram <aaviram@redhat.com>
-     * @date 30 Aug 2016
-     * @status added
+     * @author Byron Gravenorst <bgraveno@redhat.com>
+     * @date 15 Nov 2016
+     * @status updated_by_docs
      * @since 4.0.4
      */
     INITIALIZING,
 
     /**
-     * The phase where the transfer session is open, and the client can do I/O to the desired image using its preferred
+     * The phase where the transfer session is open, and the client can input or output the desired image using the preferred
      * tools.
      *
      * @author Amit Aviram <aaviram@redhat.com>
-     * @date 30 Aug 2016
-     * @status added
+     * @author Byron Gravenorst <bgraveno@redhat.com>
+     * @date 15 Nov 2016
+     * @status updated_by_docs
      * @since 4.0.4
      */
     TRANSFERRING,
 
     /**
-     * The phase where the transfer had been resumed by the client calling
-     * <<services/image_transfer/methods/resume, resume>>. Resuming will start a new session, and after calling it,
-     * the phase should be changed to `transferring`, or `paused_system` in case of a failure.
+     * The phase where the transfer has been resumed by the client calling
+     * <<services/image_transfer/methods/resume, resume>>. Resuming starts a new session, and after calling it,
+     * the phase will be changed to `transferring`, or `paused_system` in case of a failure.
      *
      * @author Amit Aviram <aaviram@redhat.com>
-     * @date 30 Aug 2016
-     * @status added
+     * @author Byron Gravenorst <bgraveno@redhat.com>
+     * @date 15 Nov 2016
+     * @status updated_by_docs
      * @since 4.0.4
      */
     RESUMING,
 
     /**
-     * This phase stands for a situation in which the session was timed out, or if some other error occurred
-     * with this transfer, e.g, ovirt-imageio-daemon is not running in the selected host for transferring. For trying
-     * to resume the session, the client should call <<services/image_transfer/methods/resume, resume>>. After
-     * resuming, the phase will change to `resuming`
+     * This phase means the session timed out, or some other error occurred
+     * with this transfer; for example ovirt-imageio-daemon is not running in the selected host.
+     * To resume the session, the client should call <<services/image_transfer/methods/resume, resume>>. After
+     * resuming, the phase will change to `resuming`.
      *
      * @author Amit Aviram <aaviram@redhat.com>
-     * @date 30 Aug 2016
-     * @status added
+     * @author Byron Gravenorst <bgraveno@redhat.com>
+     * @date 15 Nov 2016
+     * @status updated_by_docs
      * @since 4.0.4
      */
     PAUSED_SYSTEM,
 
     /**
-     * This phase will be set as a result of an active pause call by the user, using
+     * This phase is a result of a pause call by the user, using
      * <<services/image_transfer/methods/pause, pause>>.
      *
      * @author Amit Aviram <aaviram@redhat.com>
-     * @date 30 Aug 2016
-     * @status added
+     * @author Byron Gravenorst <bgraveno@redhat.com>
+     * @date 15 Nov 2016
+     * @status updated_by_docs
      * @since 4.0.4
      */
     PAUSED_USER,
 
     /**
-     * This phase will be set as a result of the user actively cancelling the transfer, which can only be performed
-     * via the webadmin.
+     * This phase will be set as a result of the user cancelling the transfer. The cancellation can only be performed
+     * in the Administration Portal.
      *
      * @author Amit Aviram <aaviram@redhat.com>
-     * @date 30 Aug 2016
-     * @status added
+     * @author Byron Gravenorst <bgraveno@redhat.com>
+     * @date 15 Nov 2016
+     * @status updated_by_docs
      * @since 4.0.4
      */
     CANCELLED,
 
     /**
      * This phase will be set when the user calls <<services/image_transfer/methods/finalize, finalize>>. Calling
-     * finalize is essential in order to finish the transfer session and using the targeted image. After finalizing,
+     * finalize is essential to finish the transfer session, and finish using the targeted image. After finalizing,
      * the phase will be changed to `finished_success` or `finished_failure`.
      *
-     * Please refer to <<services/image_transfer, image transfer>> for further documentation.
+     * Refer to <<services/image_transfer, image transfer>> for more information.
      *
      * @author Amit Aviram <aaviram@redhat.com>
-     * @date 30 Aug 2016
-     * @status added
+     * @author Byron Gravenorst <bgraveno@redhat.com>
+     * @date 15 Nov 2016
+     * @status updated_by_docs
      * @since 4.0.4
      */
     FINALIZING_SUCCESS,
 
     /**
-     * This phase can only be set by oVirt webadmin, and states that there was some error during the transfer and it
-     * is being finalized with failure.
+     * This phase can only be set in the Administration Portal, and indicates that there was an error during the transfer, and it
+     * is being finalized with a failure.
      *
      * @author Amit Aviram <aaviram@redhat.com>
-     * @date 30 Aug 2016
-     * @status added
+     * @author Byron Gravenorst <bgraveno@redhat.com>
+     * @date 17 Nov 2016
+     * @status updated_by_docs
      * @since 4.0.4
      */
     FINALIZING_FAILURE,
 
     /**
-     * States that the transfer session was successfully closed, and the targeted image was verified and ready to be
-     * used. After reaching this phase, the image transfer entity will soon be deleted.
+     * Indicates that the transfer session was successfully closed, and the targeted image was verified and ready to be
+     * used. After reaching this phase, the image transfer entity will be deleted.
      *
      * @author Amit Aviram <aaviram@redhat.com>
-     * @date 30 Aug 2016
-     * @status added
+     * @author Byron Gravenorst <bgraveno@redhat.com>
+     * @date 15 Nov 2016
+     * @status updated_by_docs
      * @since 4.0.4
      */
     FINISHED_SUCCESS,
 
     /**
-     * States that the targeted image failed the verification, and cannot be used. After reaching this phase,
-     * the image transfer entity will soon be deleted, and the targeted image will be set to illegal.
+     * Indicates that the targeted image failed the verification, and cannot be used. After reaching this phase,
+     * the image transfer entity will be deleted, and the targeted image will be set to illegal.
      *
      * @author Amit Aviram <aaviram@redhat.com>
-     * @date 30 Aug 2016
-     * @status added
+     * @author Byron Gravenorst <bgraveno@redhat.com>
+     * @date 15 Nov 2016
+     * @status updated_by_docs
      * @since 4.0.4
      */
     FINISHED_FAILURE;
