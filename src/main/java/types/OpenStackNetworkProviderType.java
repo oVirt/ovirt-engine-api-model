804ef40ef978b1a79827fb26661384247ac527da
Tahlia Richardson <trichard@redhat.com>
2016-11-16 13:16:05 +1000

    Document OpenStackNetworkProviderType.java
    Reviewed all docs items. Made some minor adjustments for tense, but otherwise they were already great.
    
    Change-Id: I81497b8a4327fa3bec66f8288685f99d01960bdf
    Signed-off-by: Tahlia Richardson <trichard@redhat.com>

--- src/main/java/types/OpenStackNetworkProviderType.java
+++ src/main/java/types/OpenStackNetworkProviderType.java
@@ -21,19 +21,31 @@ import org.ovirt.api.metamodel.annotations.Type;
 /**
  * The OpenStack network provider can either be implemented by OpenStack Neutron, in which case the Neutron
  * agent is automatically installed on the hosts, or it can be an external provider implementing the
- * OpenStack API, in which case the virtual interface driver will be a custom solution installed manually.
+ * OpenStack API, in which case the virtual interface driver is a custom solution installed manually.
+ *
+ * @author Tahlia Richardson <trichard@redhat.com>
+ * @date 16 Nov 2016
+ * @status updated_by_docs
  */
 @Type
 public enum OpenStackNetworkProviderType {
     /**
      * Indicates that the provider is OpenStack Neutron.
-     * The standard OpenStack Neutron agent will be used as the virtual interface driver.
+     * The standard OpenStack Neutron agent is used as the virtual interface driver.
+     *
+     * @author Tahlia Richardson <trichard@redhat.com>
+     * @date 16 Nov 2016
+     * @status updated_by_docs
      */
     NEUTRON,
 
     /**
      * Indicates that the provider is an external one, implementing the OpenStack Neutron API.
      * The virtual interface driver in this case is implemented by the external provider.
+     *
+     * @author Tahlia Richardson <trichard@redhat.com>
+     * @date 16 Nov 2016
+     * @status updated_by_docs
      */
     EXTERNAL;
 }
