34f61181176c36eee17ef5a366fc093a898d09e8
Tahlia Richardson <trichard@redhat.com>
2016-11-17 14:19:22 +1000

    Updated docs for OperatingSystem.java
    Reviewed and updated @Type description, reportedKernelCmdline, and customKernelCmdline
    
    Change-Id: Id1623abdee7350829a5aeb43514d7e33d2ca385a
    Signed-off-by: Tahlia Richardson <trichard@redhat.com>
    (cherry picked from commit 57ec1ff1741bc7a312fa9ce4667a3b111d5676e9)
    (cherry picked from commit f138bd812174e328b9adf296619ed4cee86f1b6c)

--- src/main/java/types/OperatingSystem.java
+++ src/main/java/types/OperatingSystem.java
@@ -19,7 +19,11 @@ package types;
 import org.ovirt.api.metamodel.annotations.Type;
 
 /**
- * Information describing the operating system. Used for virtual machines and hosts.
+ * Information describing the operating system. This is used for both virtual machines and hosts.
+ *
+ * @author Tahlia Richardson <trichard@redhat.com>
+ * @date 17 Nov 2016
+ * @status updated_by_docs
  */
 @Type
 public interface OperatingSystem {
@@ -30,27 +34,35 @@ public interface OperatingSystem {
     String cmdline();
 
     /**
-     * Host kernel command line as reported by a running host.
+     * The host kernel command line as reported by a running host.
      *
-     * Read-only attribute. Attempts to change this attribute are silently ignored.
+     * This is a read-only attribute. Attempts to change this attribute are silently ignored.
      *
      * NOTE: This attribute is currently only used for hosts.
+     *
+     * @author Tahlia Richardson <trichard@redhat.com>
+     * @date 17 Nov 2016
+     * @status updated_by_docs
      */
     String reportedKernelCmdline();
 
     /**
      * A custom part of the host kernel command line. This will be merged with the existing kernel command line.
      *
-     * You must re-install and then reboot the host to apply the changes implemented by this attribute.
+     * You must reinstall and then reboot the host to apply the changes implemented by this attribute.
      *
-     * Parameters merging: During each host deploy procedure, kernel parameters that were added
+     * During each host deploy procedure, kernel parameters that were added
      * in the previous host deploy procedure are removed using
-     * `grubby --update-kernel DEFAULT --remove-args <previous_custom_params>` and the current
+     * `grubby --update-kernel DEFAULT --remove-args <previous_custom_params>`, and the current
      * kernel command line customization is applied using
-     * `grubby --update-kernel DEFAULT --args <custom_params>`. The Engine internally keeps track of the
-     * last applied kernel parameters customization.
+     * `grubby --update-kernel DEFAULT --args <custom_params>`. The Manager internally keeps track of the
+     * last-applied kernel parameters customization.
      *
      * NOTE: This attribute is currently only used for hosts.
+     *
+     * @author Tahlia Richardson <trichard@redhat.com>
+     * @date 17 Nov 2016
+     * @status updated_by_docs
      */
     String customKernelCmdline();
     Version version();
