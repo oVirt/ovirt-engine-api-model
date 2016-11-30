0f7f02d08ab0a80bad3d1705967a67c643c42eca
Oved Ourfali <oourfali@redhat.com>
2016-11-30 17:50:59 +0200

    event and log severity types documentation.
    Change-Id: I800429cf57266d5d4fdb820c73d518cafae0ba17
    Signed-off-by: Oved Ourfali <oourfali@redhat.com>

--- src/main/java/types/Event.java
+++ src/main/java/types/Event.java
@@ -21,22 +21,158 @@ import org.ovirt.api.metamodel.annotations.Type;
 
 import java.util.Date;
 
+/**
+ * Type representing an event.
+ *
+ * @author Oved Ourfali <oourfali@redhat.com>
+ * @date 30 Nov 2016
+ * @status added
+ */
 @Type
 public interface Event extends Identified {
+    /**
+     * The event code.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 30 Nov 2016
+     * @status added
+     */
     Integer code();
+
+    /**
+     * The event severity.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 30 Nov 2016
+     * @status added
+     */
     LogSeverity severity();
+
+    /**
+     * The event time.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 30 Nov 2016
+     * @status added
+     */
     Date time();
+
+    /**
+     * The event correlation identifier. Used in order to correlate
+     * several events together.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 30 Nov 2016
+     * @status added
+     */
     String correlationId();
+
+    /**
+     * Free text identifying the origin of the event.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 30 Nov 2016
+     * @status added
+     */
     String origin();
+
+    /**
+     * A custom event identifier.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 30 Nov 2016
+     * @status added
+     */
     Integer customId();
+
+    /**
+     * Defines the flood rate.
+     * This prevents flooding in case an event appeared
+     * more than once in the defined rate.
+     * Defauls is 30 seconds.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 30 Nov 2016
+     * @status added
+     */
     Integer floodRate();
+
+    /**
+     * Free text representing custom event data.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 30 Nov 2016
+     * @status added
+     */
     String customData();
 
+    /**
+     * Reference to the user service.
+     * Event can be associated with a user.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 30 Nov 2016
+     * @status added
+     */
     @Link User user();
+
+    /**
+     * Reference to the virtual machine service.
+     * Event can be associated with a virtual machine.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 30 Nov 2016
+     * @status added
+     */
     @Link Vm vm();
+
+    /**
+     * Reference to the storage domain service.
+     * Event can be associated with a storage domain.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 30 Nov 2016
+     * @status added
+     */
     @Link StorageDomain storageDomain();
+
+    /**
+     * Reference to the host service.
+     * Event can be associated with a host.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 30 Nov 2016
+     * @status added
+     */
     @Link Host host();
+
+    /**
+     * Reference to the template service.
+     * Event can be associated with a template.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 30 Nov 2016
+     * @status added
+     */
     @Link Template template();
+
+    /**
+     * Reference to the cluster service.
+     * Event can be associated with a cluster.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 30 Nov 2016
+     * @status added
+     */
     @Link Cluster cluster();
+
+    /**
+     * Reference to the data center service.
+     * Event can be associated with a data center.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 30 Nov 2016
+     * @status added
+     */
     @Link DataCenter dataCenter();
 }
