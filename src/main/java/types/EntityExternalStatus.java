190b549f02fa45a737adb578bb3d02f4966d0526
Oved Ourfali <oourfali@redhat.com>
2016-12-01 16:54:18 +0200

    Documenting the external entity status
    Change-Id: Id586117e2c11471da472cd7a63bad91ba7699085
    Signed-off-by: Oved Ourfali <oourfali@redhat.com>

--- src/main/java/types/EntityExternalStatus.java
+++ src/main/java/types/EntityExternalStatus.java
@@ -17,11 +17,58 @@ package types;
 
 import org.ovirt.api.metamodel.annotations.Type;
 
+/**
+ * Type representing an external entity status.
+ *
+ * @author Oved Ourfali <oourfali@redhat.com>
+ * @date 01 Dec 2016
+ * @status added
+ */
 @Type
 public enum EntityExternalStatus {
+    /**
+     * The external entity status is okay.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 01 Dec 2016
+     * @status added
+     */
     OK,
+
+    /**
+     * There external entity status is okay but with some information that
+     * might be relevant.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 01 Dec 2016
+     * @status added
+     */
     INFO,
+
+    /**
+     * The external entity status is okay but with an issue that might require attention.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 01 Dec 2016
+     * @status added
+     */
     WARNING,
+
+    /**
+     * The external entity status is erronous. This might require a moderate attention.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 01 Dec 2016
+     * @status added
+     */
     ERROR,
+
+    /**
+     * The external entity has an issue that causes failures. This might require immediate attention.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 01 Dec 2016
+     * @status added
+     */
     FAILURE;
 }
