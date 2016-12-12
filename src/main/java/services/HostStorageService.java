daf0f1f2a86170724872732e5aa1c6e3eedd3040
Alexey Slaykovsky <aslaikov@redhat.com>
2016-12-12 16:17:46 +0100

    Added missing docstrings to host storage service
    Change-Id: Ic26d26e7d9e2e1d041115fdc8860a7975cd58e42
    (cherry picked from commit fab6fb9970f9d92b4afd2f70ef08519745d4e31a)

--- src/main/java/services/HostStorageService.java
+++ src/main/java/services/HostStorageService.java
@@ -22,10 +22,48 @@ import org.ovirt.api.metamodel.annotations.Out;
 import org.ovirt.api.metamodel.annotations.Service;
 import types.HostStorage;
 
+/**
+ * A service to manage host storages.
+ *
+ * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+ * @date 12 Dec 2016
+ * @status added
+ */
 @Service
 @Area("Storage")
 public interface HostStorageService {
+    /**
+     * Get list of storages.
+     *
+     * [source]
+     * ----
+     * GET /ovirt-engine/api/hosts/123/storage
+     * ----
+     *
+     * The XML response you get will be like this one:
+     *
+     * [source,xml]
+     * ----
+     * <host_storages>
+     *   <host_storage id="123">
+     *     ...
+     *   </host_storage>
+     *   ...
+     * </host_storages>
+     * ----
+     *
+     * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     interface List {
+        /**
+         * Retrieved list of storages.
+         *
+         * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+         * @date 12 Dec 2016
+         * @status added
+         */
         @Out HostStorage[] storages();
 
         /**
@@ -40,21 +78,21 @@ public interface HostStorageService {
          *
          * [source,xml]
          * ----
-         * <host_storage id="360014051136c20574f743bdbd28177fd">
+         * <host_storage id="123">
          *   <logical_units>
-         *     <logical_unit id="360014051136c20574f743bdbd28177fd">
+         *     <logical_unit id="123">
          *       <lun_mapping>0</lun_mapping>
          *       <paths>1</paths>
          *       <product_id>lun0</product_id>
-         *       <serial>SLIO-ORG_lun0_1136c205-74f7-43bd-bd28-177fd5ce6993</serial>
+         *       <serial>123</serial>
          *       <size>10737418240</size>
          *       <status>used</status>
          *       <vendor_id>LIO-ORG</vendor_id>
-         *       <volume_group_id>O9Du7I-RahN-ECe1-dZ1w-nh0b-64io-MNzIBZ</volume_group_id>
+         *       <volume_group_id>123</volume_group_id>
          *     </logical_unit>
          *   </logical_units>
          *   <type>iscsi</type>
-         *   <host id="8bb5ade5-e988-4000-8b93-dbfc6717fe50"/>
+         *   <host id="123"/>
          * </host_storage>
          * ----
          *
@@ -62,25 +100,32 @@ public interface HostStorageService {
          *
          * [source,xml]
          * ----
-         * <host_storage id="360014051136c20574f743bdbd28177fd">
+         * <host_storage id="123">
          *   <logical_units>
-         *     <logical_unit id="360014051136c20574f743bdbd28177fd">
+         *     <logical_unit id="123">
          *       <lun_mapping>0</lun_mapping>
          *       <paths>1</paths>
          *       <product_id>lun0</product_id>
-         *       <serial>SLIO-ORG_lun0_1136c205-74f7-43bd-bd28-177fd5ce6993</serial>
+         *       <serial>123</serial>
          *       <size>10737418240</size>
          *       <vendor_id>LIO-ORG</vendor_id>
-         *       <volume_group_id>O9Du7I-RahN-ECe1-dZ1w-nh0b-64io-MNzIBZ</volume_group_id>
+         *       <volume_group_id>123</volume_group_id>
          *     </logical_unit>
          *   </logical_units>
          *   <type>iscsi</type>
-         *   <host id="8bb5ade5-e988-4000-8b93-dbfc6717fe50"/>
+         *   <host id="123"/>
          * </host_storage>
          * ----
          */
         @In Boolean reportStatus();
     }
 
+    /**
+     * Reference to a service managing the storage.
+     *
+     * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Service StorageService storage(String id);
 }
