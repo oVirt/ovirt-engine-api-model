3b73756755f976962526b249737fc0d329b0680b
Sahina Bose <sabose@redhat.com>
2016-12-12 16:44:52 +0530

    Doc for GlusterBricksService
    Change-Id: I3413bbd57f4b3818c0cd21e37ebbd1aeb02ccfc0
    Signed-off-by: Sahina Bose <sabose@redhat.com>

--- src/main/java/services/gluster/GlusterBrickService.java
+++ src/main/java/services/gluster/GlusterBrickService.java
@@ -16,22 +16,99 @@ limitations under the License.
 
 package services.gluster;
 
-import annotations.Area;
 import org.ovirt.api.metamodel.annotations.In;
 import org.ovirt.api.metamodel.annotations.Out;
 import org.ovirt.api.metamodel.annotations.Service;
+
+import annotations.Area;
 import services.MeasurableService;
 import types.GlusterBrick;
 
+/**
+ * This service manages a single gluster brick.
+ *
+ * @author Sahina Bose <sabose@redhat.com>
+ * @date 12 Dec 2016
+ * @status added
+ */
 @Service
 @Area("Gluster")
 public interface GlusterBrickService extends MeasurableService {
+    /**
+     * Get details of a brick.
+     *
+     * Retrieves status details of brick from underlying gluster volume with header `All-Content` set to `true`. This is
+     * the equivalent of running `gluster volume status <volumename> <brickname> detail`.
+     *
+     * For example, to get the details of brick `234` of gluster volume `123`, send a request like this:
+     *
+     * [source]
+     * ----
+     * GET /ovirt-engine/api/clusters/567/glustervolumes/123/glusterbricks/234
+     * ----
+     *
+     * Which will return a response body like this:
+     *
+     * [source,xml]
+     * ----
+     * <brick id="234">
+     *   <name>host1:/rhgs/data/brick1</name>
+     *   <brick_dir>/rhgs/data/brick1</brick_dir>
+     *   <server_id>111</server_id>
+     *   <status>up</status>
+     *   <device>/dev/mapper/RHGS_vg1-lv_vmaddldisks</device>
+     *   <fs_name>xfs</fs_name>
+     *   <gluster_clients>
+     *     <gluster_client>
+     *       <bytes_read>2818417648</bytes_read>
+     *       <bytes_written>1384694844</bytes_written>
+     *       <client_port>1011</client_port>
+     *       <host_name>client2</host_name>
+     *     </gluster_client>
+     *   </gluster_clients>
+     *   <memory_pools>
+     *     <memory_pool>
+     *       <name>data-server:fd_t</name>
+     *       <alloc_count>1626348</alloc_count>
+     *       <cold_count>1020</cold_count>
+     *       <hot_count>4</hot_count>
+     *       <max_alloc>23</max_alloc>
+     *       <max_stdalloc>0</max_stdalloc>
+     *       <padded_size>140</padded_size>
+     *       <pool_misses>0</pool_misses>
+     *     </memory_pool>
+     *   </memory_pools>
+     *   <mnt_options>rw,seclabel,noatime,nodiratime,attr2,inode64,sunit=512,swidth=2048,noquota</mnt_options>
+     *   <pid>25589</pid>
+     *   <port>49155</port>
+     * </brick>
+     * ----
+     *
+     * @author Sahina Bose <sabose@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     interface Get {
         @Out GlusterBrick brick();
     }
 
     /**
-     * Removes this brick from the volume and deletes it from the database.
+     * Removes a brick.
+     *
+     * Removes a brick from the underlying gluster volume and deletes entries from database. This can be used only when
+     * removing a single brick without data migration. To remove multiple bricks and with data migration, use
+     * <<services/gluster_bricks/methods/migrate, migrate>> instead.
+     *
+     * For example, to delete brick `234` from gluster volume `123`, send a request like this:
+     *
+     * [source]
+     * ----
+     * DELETE /ovirt-engine/api/clusters/567/glustervolumes/123/glusterbricks/234
+     * ----
+     *
+     * @author Sahina Bose <sabose@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
      */
     interface Remove {
         /**
@@ -41,7 +118,15 @@ public interface GlusterBrickService extends MeasurableService {
     }
 
     /**
-     * Replaces this brick with a new one. The property `brick` is required.
+     * Replaces this brick with a new one.
+     *
+     * IMPORTANT: This operation has been deprecated since version 3.5 of the engine and will be removed in the future.
+     * Use <<services/gluster_bricks/methods/add, add brick(s)>> and
+     * <<services/gluster_bricks/methods/migrate, migrate brick(s)>> instead.
+     *
+     * @author Sahina Bose <sabose@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
      */
     @Deprecated
     interface Replace {
--- src/main/java/services/gluster/GlusterBricksService.java
+++ src/main/java/services/gluster/GlusterBricksService.java
@@ -16,16 +16,62 @@ limitations under the License.
 
 package services.gluster;
 
-import annotations.Area;
 import org.ovirt.api.metamodel.annotations.In;
 import org.ovirt.api.metamodel.annotations.Out;
 import org.ovirt.api.metamodel.annotations.Service;
+
+import annotations.Area;
 import types.GlusterBrick;
 
+/**
+ * This service manages the gluster bricks in a gluster volume
+ *
+ * @author Sahina Bose <sabose@redhat.com>
+ * @date 12 Dec 2016
+ * @status added
+ */
 @Service
 @Area("Gluster")
 public interface GlusterBricksService {
+    /**
+     * Activate the bricks post data migration of remove brick operation.
+     *
+     * Used to activate brick(s) once the data migration from bricks is complete but user no longer wishes to remove
+     * bricks. The bricks that were previously marked for removal will now be used as normal bricks.
+     *
+     * For example, to retain the bricks that on glustervolume `123` from which data was migrated, send a request like
+     * this:
+     *
+     * [source]
+     * ----
+     * POST /ovirt-engine/api/clusters/567/glustervolumes/123/glusterbricks/activate
+     * ----
+     *
+     * With a request body like this:
+     *
+     * [source,xml]
+     * ----
+     * <action>
+     *   <bricks>
+     *     <brick>
+     *       <name>host1:/rhgs/brick1</name>
+     *     </brick>
+     *   </bricks>
+     * </action>
+     * ----
+     *
+     * @author Sahina Bose <sabose@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     interface Activate {
+        /**
+         * The list of bricks that need to be re-activated.
+         *
+         * @author Sahina Bose <sabose@redhat.com>
+         * @date 12 Dec 2016
+         * @status added
+         */
         @In GlusterBrick[] bricks();
 
         /**
@@ -35,16 +81,98 @@ public interface GlusterBricksService {
     }
 
     /**
-     * Adds given list of bricks to the volume, and updates the database accordingly. The properties `serverId` and
-     * `brickDir`are required.
+     * Adds a list of bricks to gluster volume.
+     *
+     * Used to expand a gluster volume by adding bricks. For replicated volume types, the parameter `replica_count`
+     * needs to be passed. In case the replica count is being increased, then the number of bricks needs to be
+     * equivalent to the number of replica sets.
+     *
+     * For example, to add bricks to gluster volume `123`, send a request like this:
+     *
+     * [source]
+     * ----
+     * POST /ovirt-engine/api/clusters/567/glustervolumes/123/glusterbricks
+     * ----
+     *
+     * With a request body like this:
+     *
+     * [source,xml]
+     * ----
+     * <bricks>
+     *   <brick>
+     *     <server_id>111</server_id>
+     *     <brick_dir>/export/data/brick3</brick_dir>
+     *   </brick>
+     * </bricks>
+     * ----
+     *
+     * @author Sahina Bose <sabose@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
      */
     interface Add {
         /**
          * The list of bricks to be added to the volume
+         *
+         * @author Sahina Bose <sabose@redhat.com>
+         * @date 12 Dec 2016
+         * @status added
          */
         @In @Out GlusterBrick[] bricks();
+
+        /**
+         * Replica count of volume post add operation.
+         *
+         * @author Sahina Bose <sabose@redhat.com>
+         * @date 12 Dec 2016
+         * @status added
+         */
+        @In Integer replicaCount();
+
+        /**
+         * Stripe count of volume post add operation.
+         *
+         * @author Sahina Bose <sabose@redhat.com>
+         * @date 12 Dec 2016
+         * @status added
+         */
+        @In Integer stripeCount();
     }
 
+    /**
+     * Lists the bricks of a gluster volume.
+     *
+     * For example, to list bricks of gluster volume `123`, send a request like this:
+     *
+     * [source]
+     * ----
+     * GET /ovirt-engine/api/clusters/567/glustervolumes/123/glusterbricks
+     * ----
+     *
+     * Provides an output as below:
+     *
+     * [source,xml]
+     * ----
+     * <bricks>
+     *   <brick id="234">
+     *     <name>host1:/rhgs/data/brick1</name>
+     *     <brick_dir>/rhgs/data/brick1</brick_dir>
+     *     <server_id>111</server_id>
+     *     <status>up</status>
+     *   </brick>
+     *   <brick id="233">
+     *     <name>host2:/rhgs/data/brick1</name>
+     *     <brick_dir>/rhgs/data/brick1</brick_dir>
+     *     <server_id>222</server_id>
+     *     <status>up</status>
+     *   </brick>
+     * </bricks>
+     * ----
+     *
+     * @author Sahina Bose <sabose@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     interface List {
         @Out GlusterBrick[] bricks();
 
@@ -54,17 +182,86 @@ public interface GlusterBricksService {
         @In Integer max();
     }
 
+    /**
+     * Start migration of data prior to removing bricks.
+     *
+     * Removing bricks is a two-step process, where the data on bricks to be removed, is first migrated to remaining
+     * bricks. Once migration is completed the removal of bricks is confirmed via the API
+     * <<services/gluster_bricks/methods/remove, remove>>. If at any point, the action needs to be cancelled
+     * <<services/gluster_bricks/methods/stopmigrate, stopmigrate>> has to be called.
+     *
+     * For instance, to delete a brick from a gluster volume with id `123`, send a request:
+     *
+     * [source]
+     * ----
+     * POST /ovirt-engine/api/clusters/567/glustervolumes/123/glusterbricks/migrate
+     * ----
+     *
+     * With a request body like this:
+     *
+     * [source,xml]
+     * ----
+     * <action>
+     *   <bricks>
+     *     <brick>
+     *       <name>host1:/rhgs/brick1</name>
+     *     </brick>
+     *   </bricks>
+     * </action>
+     * ----
+     *
+     * The migration process can be tracked from the job id returned from the API using
+     * <<services/job/methods/get, job>> and steps in job using <<services/step/methods/get, step>>
+     *
+     * @author Sahina Bose <sabose@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     interface Migrate {
+        /**
+         * List of bricks for which data migration needs to be started.
+         *
+         * @author Sahina Bose <sabose@redhat.com>
+         * @date 12 Dec 2016
+         * @status added
+         */
         @In GlusterBrick[] bricks();
 
         /**
          * Indicates if the migration should be performed asynchronously.
          */
         @In Boolean async();
+
     }
 
     /**
-     * Removes the given list of bricks brick from the volume and deletes them from the database.
+     * Removes bricks from gluster volume.
+     *
+     * The recommended way to remove bricks without data loss is to first migrate the data using
+     * <<services/gluster_bricks/methods/stopmigrate, stopmigrate>> and then removing them. If migrate was not called on
+     * bricks prior to remove, the bricks are removed without data migration which may lead to data loss.
+     *
+     * For example, to delete the bricks from gluster volume `123`, send a request like this:
+     *
+     * [source]
+     * ----
+     * DELETE /ovirt-engine/api/clusters/567/glustervolumes/123/glusterbricks
+     * ----
+     *
+     * With a request body like this:
+     *
+     * [source,xml]
+     * ----
+     * <bricks>
+     *   <brick>
+     *     <name>host:brick_directory</name>
+     *   </brick>
+     * </bricks>
+     * ----
+     *
+     * @author Sahina Bose <sabose@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
      */
     interface Remove {
         /**
@@ -73,12 +270,58 @@ public interface GlusterBricksService {
         @In GlusterBrick[] bricks();
 
         /**
+         * Replica count of volume post add operation.
+         *
+         * @author Sahina Bose <sabose@redhat.com>
+         * @date 12 Dec 2016
+         * @status added
+         */
+        @In Integer replicaCount();
+
+        /**
          * Indicates if the remove should be performed asynchronously.
          */
         @In Boolean async();
     }
 
+    /**
+     * Stops migration of data from bricks for a remove brick operation.
+     *
+     * To cancel data migration that was started as part of the 2-step remove brick process in case the user wishes to
+     * continue using the bricks. The bricks that were marked for removal will function as normal bricks post this
+     * operation.
+     *
+     * For example, to stop migration of data from the bricks of gluster volume `123`, send a request like this:
+     *
+     * [source]
+     * ----
+     * POST /ovirt-engine/api/clusters/567/glustervolumes/123/glusterbricks/stopmigrate
+     * ----
+     *
+     * With a request body like this:
+     *
+     * [source,xml]
+     * ----
+     * <bricks>
+     *   <brick>
+     *     <name>host:brick_directory</name>
+     *   </brick>
+     * </bricks>
+     * ----
+     *
+     * @author Sahina Bose <sabose@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     interface StopMigrate {
+        /**
+         * List of bricks for which data migration needs to be stopped. This list should match the arguments passed to
+         * <<services/gluster_bricks/methods/migrate, migrate>>.
+         *
+         * @author Sahina Bose <sabose@redhat.com>
+         * @date 12 Dec 2016
+         * @status added
+         */
         @In GlusterBrick[] bricks();
 
         /**
@@ -87,5 +330,12 @@ public interface GlusterBricksService {
         @In Boolean async();
     }
 
+    /**
+     * Returns a reference to the service managing a single gluster brick.
+     *
+     * @author Sahina Bose <sabose@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Service GlusterBrick brick(String id);
 }
