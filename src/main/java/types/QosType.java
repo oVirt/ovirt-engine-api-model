5fd3ce51ddeabfa0d526c2708902f180272b782b
Dominik Holler <dholler@redhat.com>
2016-12-12 11:21:08 +0100

    Added the documentation for the QosType type.
    Change-Id: I38715fd507431035c73da9576f12eb55d969a85b
    Signed-off-by: Dominik Holler <dholler@redhat.com>

--- src/main/java/types/QosType.java
+++ src/main/java/types/QosType.java
@@ -17,10 +17,48 @@ package types;
 
 import org.ovirt.api.metamodel.annotations.Type;
 
+/**
+ * This type represents the kind of resource the <<types/qos,Quality of service (QoS)>> can be assigned to.
+ *
+ * @author Dominik Holler <dholler@redhat.com>
+ * @date 12 Dec 2016
+ * @status added
+ */
 @Type
 public enum QosType {
+    /**
+     * The <<types/qos,Quality of service (QoS)>> can be assigned to storage.
+     *
+     * @author Dominik Holler <dholler@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     STORAGE,
+
+    /**
+     * The <<types/qos,Quality of service (QoS)>> can be assigned to resources with computing capabilities.
+     *
+     * @author Dominik Holler <dholler@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     CPU,
+
+    /**
+     * The <<types/qos,Quality of service (QoS)>> can be assigned to virtual machines networks.
+     *
+     * @author Dominik Holler <dholler@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     NETWORK,
+
+    /**
+     * The <<types/qos,Quality of service (QoS)>> can be assigned to host networks.
+     *
+     * @author Dominik Holler <dholler@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     HOSTNETWORK;
 }
