18aeb843bd3a26ba666edd3bdbee5c97912cc90b
Piotr Kliczewski <piotr.kliczewski@gmail.com>
2016-12-12 12:30:22 +0100

    EntryPoint doc type
    Change-Id: I1ebfc328a2f9747d50668286a1dc27252cabb6cf
    Signed-off-by: Piotr Kliczewski <piotr.kliczewski@gmail.com>
    (cherry picked from commit a09facb994dfc41cd82b00b7610f902e66fff9d1)

--- src/main/java/types/ProductInfo.java
+++ src/main/java/types/ProductInfo.java
@@ -19,7 +19,37 @@ package types;
 import org.ovirt.api.metamodel.annotations.Type;
 
 /**
- * This type contains information about the product, including its name, the name of the vendor, and the version.
+ * Product information.
+ *
+ * The entry point contains a `product_info` element to help an API user determine the legitimacy of the
+ * {product-name} environment. This includes the name of the product, the `vendor` and the `version`.
+ *
+ * Verify a genuine {product-name} environment
+ *
+ * The follow elements identify a genuine {product-name} environment:
+ *
+ * [source]
+ * ----
+ * <api>
+ * ...
+ * <product_info>
+ *   <name>oVirt Engine</name>
+ *   <vendor>ovirt.org</vendor>
+ *   <version>
+ *     <build>0</build>
+ *     <full_version>4.1.0_master</full_version>
+ *     <major>4</major>
+ *     <minor>1</minor>
+ *     <revision>0</revision>
+ *   </version>
+ * </product_info>
+ * ...
+ * </api>
+ * ----
+ *
+ * @author Piotr Kliczewski <pkliczew@redhat.com>
+ * @date 12 Dec 2016
+ * @status added
  */
 @Type
 public interface ProductInfo {
