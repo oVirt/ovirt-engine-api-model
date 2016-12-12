3ff682396e997e1497f6140ec33ac7c1591c29aa
Ravi Nori <rnori@redhat.com>
2016-12-12 11:46:34 -0500

    Add documentation to DomainService
    Change-Id: I7df749dde90c7cedbd8b60ae835b74f8ac594452
    Signed-off-by: Ravi Nori <rnori@redhat.com>

--- src/main/java/services/aaa/DomainService.java
+++ src/main/java/services/aaa/DomainService.java
@@ -21,13 +21,68 @@ import org.ovirt.api.metamodel.annotations.Out;
 import org.ovirt.api.metamodel.annotations.Service;
 import types.Domain;
 
+/**
+ * A service to view details of an authentication domain in the system.
+ *
+ * @author Ravi Nori <rnori@redhat.com>
+ * @date 12 Dec 2016
+ * @status added
+ */
 @Service
 @Area("Infrastructure")
 public interface DomainService {
+    /**
+     * Gets the authentication domain information.
+     *
+     * Usage:
+     *
+     * ....
+     * GET /ovirt-engine/api/domains/5678
+     * ....
+     *
+     * Will return the domain information:
+     *
+     * [source,xml]
+     * ----
+     * <domain href="/ovirt-engine/api/domains/5678" id="5678">
+     *   <name>internal-authz</name>
+     *   <link href="/ovirt-engine/api/domains/5678/users" rel="users"/>
+     *   <link href="/ovirt-engine/api/domains/5678/groups" rel="groups"/>
+     *   <link href="/ovirt-engine/api/domains/5678/users?search={query}" rel="users/search"/>
+     *   <link href="/ovirt-engine/api/domains/5678/groups?search={query}" rel="groups/search"/>
+     * </domain>
+     * ----
+     *
+     * @author Ravi Nori <rnori@redhat.com>
+     * @date 12 Dec 2017
+     * @status added
+     */
     interface Get {
+        /**
+         * The authentication domain.
+         *
+         * @author Ravi Nori <rnori@redhat.com>
+         * @date 12 Dec 2017
+         * @status added
+         */
         @Out Domain domain();
     }
 
+    /**
+     * Reference to a service to manage domain users.
+     *
+     * @author Ravi Nori <rnori@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Service DomainUsersService users();
+
+    /**
+     * Reference to a service to manage domain groups.
+     *
+     * @author Ravi Nori <rnori@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Service DomainGroupsService groups();
 }
