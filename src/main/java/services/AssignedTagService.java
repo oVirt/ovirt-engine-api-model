56a611753fb4cfef3bd7bafd23a4a37fa1bf4504
Ondra Machacek <omachace@redhat.com>
2016-12-12 14:48:33 +0100

    Add documentation for tags
    This patch documents all the services and types to manage tags in the
    system, including all its attributes and actions of the services.
    
    Change-Id: I0d5b6a6356c17e4dd40d11521ddac49742804435
    Signed-off-by: Ondra Machacek <omachace@redhat.com>
    (cherry picked from commit 9b432f78df33a09fb99b624c7c94cd3c9663c0ba)

--- src/main/java/services/AssignedTagService.java
+++ src/main/java/services/AssignedTagService.java
@@ -22,13 +22,64 @@ import org.ovirt.api.metamodel.annotations.Out;
 import org.ovirt.api.metamodel.annotations.Service;
 import types.Tag;
 
+/**
+ * A service to manage assignment of specific tag to specific entities in system.
+ *
+ * @author Ondra Machacek <omachace@redhat.com>
+ * @date 12 Dec 2016
+ * @status added
+ */
 @Service
 @Area("Infrastructure")
 public interface AssignedTagService {
+
+    /**
+     * Gets the information about the assigned tag.
+     *
+     * For example to retrieve the information about the tag with the id `456` which is assigned to virtual machine
+     * with id `123` send a request like this:
+     *
+     * ....
+     * GET /ovirt-engine/api/vms/123/tags/456
+     * ....
+     *
+     * [source,xml]
+     * ----
+     * <tag href="/ovirt-engine/api/tags/456" id="456">
+     *   <name>root</name>
+     *   <description>root</description>
+     *   <vm href="/ovirt-engine/api/vms/123" id="123"/>
+     * </tag>
+     * ----
+     *
+     * @author Ondra Machacek <omachace@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     interface Get {
+        /**
+         * The assigned tag.
+         *
+         * @author Ondra Machacek <omachace@redhat.com>
+         * @date 12 Dec 2016
+         * @status added
+         */
         @Out Tag tag();
     }
 
+    /**
+     * Unassign tag from specific entity in the system.
+     *
+     * For example to unassign the tag with id `456` from virtual machine with id `123` send a request like this:
+     *
+     * ....
+     * DELETE /ovirt-engine/api/vms/123/tags/456
+     * ....
+     *
+     * @author Ondra Machacek <omachace@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     interface Remove {
         /**
          * Indicates if the remove should be performed asynchronously.
