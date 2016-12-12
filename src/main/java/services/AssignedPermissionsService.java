139a3c35052dd54eed01dd7f470eb7046cc394fa
Ondra Machacek <omachace@redhat.com>
2016-12-12 11:33:30 +0100

    Add documentation to AssignedPermissionsService
    This patch documents an actions which can be achieved with
    AssignedPermissionsService.
    
    Change-Id: Ia872fa4aabc3d1e4e0574023f3286b82bc7dba21
    Signed-off-by: Ondra Machacek <omachace@redhat.com>

--- src/main/java/services/AssignedPermissionsService.java
+++ src/main/java/services/AssignedPermissionsService.java
@@ -23,16 +23,128 @@ import org.ovirt.api.metamodel.annotations.Service;
 import types.Permission;
 
 /**
- * Represents a permission sub-collection, scoped by User or some entity type.
+ * Represents a permission sub-collection, scoped by user, group or some entity type.
+ *
+ * @author Ondra Machacek <omachace@redhat.com>
+ * @date 12 Dec 2016
+ * @status added
  */
 @Service
 @Area("Infrastructure")
 public interface AssignedPermissionsService {
+
+    /**
+     * Assign a new permission to a user or group for specific entity.
+     *
+     * For example, to assign the `UserVmManager` role to the virtual machine with id `123` to the user with id `456`
+     * send a request like this:
+     *
+     * ....
+     * POST /ovirt-engine/api/vms/123/permissions
+     * ....
+     *
+     * With a request body like this:
+     *
+     * [source,xml]
+     * ----
+     * <permission>
+     *   <role>
+     *     <name>UserVmManager</name>
+     *   </role>
+     *   <user id="456"/>
+     * </permission>
+     * ----
+     *
+     * To assign the `SuperUser` role to the system to the user with id `456` send a request like this:
+     *
+     * ....
+     * POST /ovirt-engine/api/permissions
+     * ....
+     *
+     * With a request body like this:
+     *
+     * [source,xml]
+     * ----
+     * <permission>
+     *   <role>
+     *     <name>SuperUser</name>
+     *   </role>
+     *   <user id="456"/>
+     * </permission>
+     * ----
+     *
+     * If you want to assign permission to the group instead of the user please replace the `user` element with the
+     * `group` element with proper `id` of the group. For example to assign the `UserRole` role to the cluster with
+     * id `123` to the group with id `789` send a request like this:
+     *
+     * ....
+     * POST /ovirt-engine/api/clusters/123/permissions
+     * ....
+     *
+     * With a request body like this:
+     *
+     * [source,xml]
+     * ----
+     * <permission>
+     *   <role>
+     *     <name>UserRole</name>
+     *   </role>
+     *   <group id="789"/>
+     * </permission>
+     * ----
+     *
+     * @author Ondra Machacek <omachace@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     interface Add {
+        /**
+         * The permission.
+         *
+         * @author Ondra Machacek <omachace@redhat.com>
+         * @date 12 Dec 2016
+         * @status added
+         */
         @In @Out Permission permission();
     }
 
+    /**
+     * List all the permissions of the specific entity.
+     *
+     * For example to list all the permissions of the cluster with id `123` send a request like this:
+     *
+     * ....
+     * GET /ovirt-engine/api/clusters/123/permissions
+     * ....
+     *
+     * [source,xml]
+     * ----
+     * <permissions>
+     *   <permission id="456">
+     *     <cluster id="123"/>
+     *     <role id="789"/>
+     *     <user id="451"/>
+     *   </permission>
+     *   <permission id="654">
+     *     <cluster id="123"/>
+     *     <role id="789"/>
+     *     <group id="127"/>
+     *   </permission>
+     * </permissions>
+     * ----
+     *
+     * @author Ondra Machacek <omachace@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     interface List {
+        /**
+         * The list of permissions.
+         *
+         * @author Ondra Machacek <omachace@redhat.com>
+         * @date 12 Dec 2016
+         * @status added
+         */
         @Out Permission[] permissions();
     }
 
