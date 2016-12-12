cdeb68952c6d40dd1576406fd41ba6c70d6e332c
Alexey Slaykovsky <aslaikov@redhat.com>
2016-12-12 15:01:22 +0100

    Added missing docstring for a permission type
    Change-Id: I6c2d6849fe8db9fa6079285ac515d7e445b9cf9b
    Signed-off-by: Aleksei Slaikovskii <aslaikov@redhat.com>
    (cherry picked from commit 1240a756eb9e214ec5be7c9126ada02552d2986a)

--- src/main/java/types/Permission.java
+++ src/main/java/types/Permission.java
@@ -19,17 +19,111 @@ package types;
 import org.ovirt.api.metamodel.annotations.Link;
 import org.ovirt.api.metamodel.annotations.Type;
 
+/**
+ * Type represents a permission.
+ *
+ * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+ * @date 12 Dec 2016
+ * @status added
+ */
 @Type
 public interface Permission extends Identified {
+    /**
+     * Reference to role.
+     *
+     * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Link Role role();
+
+    /**
+     * Reference to user.
+     *
+     * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Link User user();
+
+    /**
+     * Reference to group.
+     *
+     * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Link Group group();
+
+    /**
+     * Reference to data center.
+     *
+     * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Link DataCenter dataCenter();
+
+    /**
+     * Reference to cluster.
+     *
+     * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Link Cluster cluster();
+
+    /**
+     * Reference to host.
+     *
+     * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Link Host host();
+
+    /**
+     * Reference to storage domain.
+     *
+     * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Link StorageDomain storageDomain();
+
+    /**
+     * Reference to virtual machine.
+     *
+     * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Link Vm vm();
+
+    /**
+     * Reference to virtual machines pool.
+     *
+     * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Link VmPool vmPool();
+
+    /**
+     * Reference to template.
+     *
+     * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Link Template template();
+
+    /**
+     * Reference to disk.
+     *
+     * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Link Disk disk();
 }
