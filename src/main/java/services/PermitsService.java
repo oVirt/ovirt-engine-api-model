c62441ecf71504f091778d7ba202c2a87368e3a3
Ondra Machacek <omachace@redhat.com>
2016-12-12 12:35:06 +0100

    Add documentation to PermitsService
    This patch adds documentation for PermitsService, describing
    how to add and list permits of the role.
    
    Change-Id: Idc4ae3072507785c87eb930eadd6e06b7a2622af
    Signed-off-by: Ondra Machacek <omachace@redhat.com>
    (cherry picked from commit aa84811fcb4a8badf4076ad5d4b015b3743d08a9)

--- src/main/java/services/PermitsService.java
+++ src/main/java/services/PermitsService.java
@@ -22,12 +22,37 @@ import org.ovirt.api.metamodel.annotations.Out;
 import org.ovirt.api.metamodel.annotations.Service;
 import types.Permit;
 
+/**
+ * Represents a permits sub-collection of the specific role.
+ *
+ * @author Ondra Machacek <omachace@redhat.com>
+ * @date 12 Dec 2016
+ * @status added
+ */
 @Service
 @Area("Infrastructure")
 public interface PermitsService {
     /**
-     * Adds a permit to the set aggregated by parent role. The permit must be one retrieved from the capabilities
-     * resource.
+     * Adds a permit to the role. The permit name can be retrieved from the <<services/cluster_levels>> service.
+     *
+     * For example to assign a permit `create_vm` to the role with id `123` send a request like this:
+     *
+     * ....
+     * POST /ovirt-engine/api/roles/123/permits
+     * ....
+     *
+     * With a request body like this:
+     *
+     * [source,xml]
+     * ----
+     * <permit>
+     *   <name>create_vm</name>
+     * </permit>
+     * ----
+     *
+     * @author Ondra Machacek <omachace@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
      */
     interface Add {
         /**
@@ -37,7 +62,43 @@ public interface PermitsService {
 
     }
 
+    /**
+     * List the permits of the role.
+     *
+     * For example to list the permits of the role with the id `123` send a request like this:
+     *
+     * ....
+     * GET /ovirt-engine/api/roles/123/permits
+     * ....
+     *
+     * [source,xml]
+     * ----
+     * <permits>
+     *   <permit href="/ovirt-engine/api/roles/123/permits/5" id="5">
+     *     <name>change_vm_cd</name>
+     *     <administrative>false</administrative>
+     *     <role href="/ovirt-engine/api/roles/123" id="123"/>
+     *   </permit>
+     *   <permit href="/ovirt-engine/api/roles/123/permits/7" id="7">
+     *     <name>connect_to_vm</name>
+     *     <administrative>false</administrative>
+     *     <role href="/ovirt-engine/api/roles/123" id="123"/>
+     *   </permit>
+     * </permits>
+     * ----
+     *
+     * @author Ondra Machacek <omachace@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     interface List {
+        /**
+         * List of permits.
+         *
+         * @author Ondra Machacek <omachace@redhat.com>
+         * @date 12 Dec 2016
+         * @status added
+         */
         @Out Permit[] permits();
 
         /**
