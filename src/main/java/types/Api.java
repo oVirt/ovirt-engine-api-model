a09facb994dfc41cd82b00b7610f902e66fff9d1
Piotr Kliczewski <piotr.kliczewski@gmail.com>
2016-12-12 12:30:22 +0100

    EntryPoint doc type
    Change-Id: I1ebfc328a2f9747d50668286a1dc27252cabb6cf
    Signed-off-by: Piotr Kliczewski <piotr.kliczewski@gmail.com>

--- src/main/java/types/Api.java
+++ src/main/java/types/Api.java
@@ -21,7 +21,62 @@ import java.util.Date;
 import org.ovirt.api.metamodel.annotations.Type;
 
 /**
- * This type contains the information returned by root service of the API.
+ * The summary of the API.
+ *
+ * To get the summary of the API send a request like this:
+ *
+ * ....
+ * GET /ovirt-engine/api
+ * ....
+ *
+ * The result will be like this:
+ *
+ * [source,xml]
+ * ----
+ * <api>
+ *   <link rel="hosts" href="/ovirt-engine/api/hosts"/>
+ *   <link rel="vms" href="/ovirt-engine/api/vms"/>
+ *   ...
+ *   <product_info>
+ *     <name>oVirt Engine</name>
+ *     <vendor>ovirt.org</vendor>
+ *     <version>
+ *       <build>0</build>
+ *       <full_version>4.1.0_master</full_version>
+ *       <major>4</major>
+ *       <minor>1</minor>
+ *       <revision>0</revision>
+ *     </version>
+ *   </product_info>
+ *   <special_objects>
+ *     <link rel="templates/blank" href="..."/>
+ *     <link rel="tags/root" href="..."/>
+ *   </special_objects>
+ *   <summary>
+ *     <vms>
+ *       <total>10</total>
+ *       <active>3</active>
+ *     </vms>
+ *     <hosts>
+ *       <total>2</total>
+ *       <active>2</active>
+ *     </hosts>
+ *     <users>
+ *       <total>8</total>
+ *       <active>2</active>
+ *     </users>
+ *     <storage_domains>
+ *       <total>2</total>
+ *       <active>2</active>
+ *     </storage_domains>
+ *   </summary>
+ *   <time>2016-12-12T12:22:25.866+01:00</time>
+ * </api>
+ * ----
+ *
+ * @author Piotr Kliczewski <pkliczew@redhat.com>
+ * @date 12 Dec 2016
+ * @status added
  */
 @Type
 public interface Api {
