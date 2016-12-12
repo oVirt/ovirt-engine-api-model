6984f3b5ab748265e2e511a2c172b7ecd9ece8eb
Milan Zamazal <mzamazal@redhat.com>
2016-12-12 17:07:42 +0100

    docs: Documentation added to VmWatchdog*Service
    Change-Id: Id988ca49e7db718c1776941262545b1899e43572
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>
    (cherry picked from commit 0129b12e44c81a18b9f38d91f1927ff7b3c2a887)

--- src/main/java/services/VmWatchdogService.java
+++ src/main/java/services/VmWatchdogService.java
@@ -22,14 +22,90 @@ import org.ovirt.api.metamodel.annotations.Out;
 import org.ovirt.api.metamodel.annotations.Service;
 import types.Watchdog;
 
+/**
+ * A service managing a watchdog on virtual machines.
+ *
+ * @author Milan Zamazal <mzamazal@redhat.com>
+ * @date 12 Dec 2016
+ * @status added
+ */
 @Service
 @Area("Virtualization")
 public interface VmWatchdogService {
+    /**
+     * Returns the information about the watchdog.
+     *
+     * @author Milan Zamazal <mzamazal@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     interface Get {
+        /**
+         * The information about the watchdog.
+         *
+         * The information consists of `model` element, `action` element and the reference to the
+         * virtual machine. It may look like this:
+         *
+         * [source,xml]
+         * ----
+         * <watchdogs>
+         *   <watchdog href="/ovirt-engine/api/vms/123/watchdogs/00000000-0000-0000-0000-000000000000" id="00000000-0000-0000-0000-000000000000">
+         *     <vm href="/ovirt-engine/api/vms/123" id="123"/>
+         *     <action>poweroff</action>
+         *     <model>i6300esb</model>
+         *   </watchdog>
+         * </watchdogs>
+         * ----
+         *
+         * @author Milan Zamazal <mzamazal@redhat.com>
+         * @date 12 Dec 2016
+         * @status added
+         */
         @Out Watchdog watchdog();
     }
 
+    /**
+     * Updates the information about the watchdog.
+     *
+     * You can update the information using `action` and `model` elements.
+     *
+     * For example, to update a watchdog, send a request like this:
+     *
+     * [source]
+     * ----
+     * PUT /ovirt-engine/api/vms/123/watchdogs
+     * <watchdog>
+     *   <action>reset</action>
+     * </watchdog>
+     * ----
+     *
+     * with response body:
+     *
+     * [source,xml]
+     * ----
+     * <watchdog href="/ovirt-engine/api/vms/123/watchdogs/00000000-0000-0000-0000-000000000000" id="00000000-0000-0000-0000-000000000000">
+     *   <vm href="/ovirt-engine/api/vms/123" id="123"/>
+     *   <action>reset</action>
+     *   <model>i6300esb</model>
+     * </watchdog>
+     * ----
+     *
+     * @author Milan Zamazal <mzamazal@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     interface Update {
+        /**
+         * The information about the watchdog.
+         *
+         * The request data must contain at least one of `model` and `action`
+         * elements. The response data contains complete information about the
+         * updated watchdog.
+         *
+         * @author Milan Zamazal <mzamazal@redhat.com>
+         * @date 12 Dec 2016
+         * @status added
+         */
         @In @Out Watchdog watchdog();
 
         /**
@@ -38,6 +114,20 @@ public interface VmWatchdogService {
         @In Boolean async();
     }
 
+    /**
+     * Removes the watchdog from the virtual machine.
+     *
+     * For example, to remove a watchdog from a virtual machine, send a request like this:
+     *
+     * [source]
+     * ----
+     * DELETE /ovirt-engine/api/vms/123/watchdogs/00000000-0000-0000-0000-000000000000
+     * ----
+     *
+     * @author Milan Zamazal <mzamazal@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     interface Remove {
         /**
          * Indicates if the remove should be performed asynchronously.
--- src/main/java/services/VmWatchdogsService.java
+++ src/main/java/services/VmWatchdogsService.java
@@ -22,14 +22,89 @@ import org.ovirt.api.metamodel.annotations.Out;
 import org.ovirt.api.metamodel.annotations.Service;
 import types.Watchdog;
 
+/**
+ * Lists the watchdogs of a virtual machine.
+ *
+ * @author Milan Zamazal <mzamazal@redhat.com>
+ * @date 12 Dec 2016
+ * @status added
+ */
 @Service
 @Area("Virtualization")
 public interface VmWatchdogsService {
+    /**
+     * Adds new watchdog to the virtual machine.
+     *
+     * For example, to add a watchdog to a virtual machine, send a request like this:
+     *
+     * [source]
+     * ----
+     * POST /ovirt-engine/api/vms/123/watchdogs
+     * <watchdog>
+     *   <action>poweroff</action>
+     *   <model>i6300esb</model>
+     * </watchdog>
+     * ----
+     *
+     * with response body:
+     *
+     * [source,xml]
+     * ----
+     * <watchdog href="/ovirt-engine/api/vms/123/watchdogs/00000000-0000-0000-0000-000000000000" id="00000000-0000-0000-0000-000000000000">
+     *   <vm href="/ovirt-engine/api/vms/123" id="123"/>
+     *   <action>poweroff</action>
+     *   <model>i6300esb</model>
+     * </watchdog>
+     * ----
+     *
+     * @author Milan Zamazal <mzamazal@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     interface Add {
+        /**
+         * The information about the watchdog.
+         *
+         * The request data must contain `model` element (such as `i6300esb`) and `action` element
+         * (one of `none`, `reset`, `poweroff`, `dump`, `pause`). The response data additionally
+         * contains references to the added watchdog and to the virtual machine.
+         *
+         * @author Milan Zamazal <mzamazal@redhat.com>
+         * @date 12 Dec 2016
+         * @status added
+         */
         @In @Out Watchdog watchdog();
     }
 
+    /**
+     * The list of watchdogs of the virtual machine.
+     *
+     * @author Milan Zamazal <mzamazal@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     interface List {
+        /**
+         * The information about the watchdog.
+         *
+         * The information consists of `model` element, `action` element and the reference to the
+         * virtual machine. It may look like this:
+         *
+         * [source,xml]
+         * ----
+         * <watchdogs>
+         *   <watchdog href="/ovirt-engine/api/vms/123/watchdogs/00000000-0000-0000-0000-000000000000" id="00000000-0000-0000-0000-000000000000">
+         *     <vm href="/ovirt-engine/api/vms/123" id="123"/>
+         *     <action>poweroff</action>
+         *     <model>i6300esb</model>
+         *   </watchdog>
+         * </watchdogs>
+         * ----
+         *
+         * @author Milan Zamazal <mzamazal@redhat.com>
+         * @date 12 Dec 2016
+         * @status added
+         */
         @Out Watchdog[] watchdogs();
 
         /**
@@ -38,5 +113,12 @@ public interface VmWatchdogsService {
         @In Integer max();
     }
 
+    /**
+     * Returns a reference to the service that manages a specific watchdog.
+     *
+     * @author Milan Zamazal <mzamazal@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Service VmWatchdogService watchdog(String id);
 }
