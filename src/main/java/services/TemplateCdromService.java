841e50846fc06bb7b0bb28f7a0d7e41f48d993b5
Milan Zamazal <mzamazal@redhat.com>
2016-12-12 13:06:18 +0100

    docs: Documentation added to TemplateCdrom*Service
    Change-Id: I35a195576b2e355ecaaed5465219825f154fa061
    Signed-off-by: Milan Zamazal <mzamazal@redhat.com>

--- src/main/java/services/TemplateCdromService.java
+++ src/main/java/services/TemplateCdromService.java
@@ -21,10 +21,60 @@ import org.ovirt.api.metamodel.annotations.Out;
 import org.ovirt.api.metamodel.annotations.Service;
 import types.Cdrom;
 
+/**
+ * A service managing a CD-ROM device on templates.
+ *
+ * @author Milan Zamazal <mzamazal@redhat.com>
+ * @date 12 Dec 2016
+ * @status added
+ */
 @Service
 @Area("Virtualization")
 public interface TemplateCdromService {
+    /**
+     * Returns the information about this CD-ROM device.
+     *
+     * For example, to get information about the CD-ROM device of template `123` send a request like:
+     *
+     * [source]
+     * ----
+     * GET /ovirt-engine/api/templates/123/cdroms/
+     * ----
+     *
+     * @author Milan Zamazal <mzamazal@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     interface Get {
+        /**
+         * The information about the CD-ROM device.
+         *
+         * The information consists of `cdrom` attribute containing reference to the CD-ROM device, the template,
+         * and optionally the inserted disk.
+         *
+         * If there is a disk inserted then the `file` attribute will contain a reference to the ISO image:
+         *
+         * [source,xml]
+         * ----
+         * <cdrom href="..." id="00000000-0000-0000-0000-000000000000">
+         *   <template href="/ovirt-engine/api/templates/123" id="123"/>
+         *   <file id="mycd.iso"/>
+         * </cdrom>
+         * ----
+         *
+         * If there is no disk inserted then the `file` attribute won't be reported:
+         *
+         * [source,xml]
+         * ----
+         * <cdrom href="..." id="00000000-0000-0000-0000-000000000000">
+         *   <template href="/ovirt-engine/api/templates/123" id="123"/>
+         * </cdrom>
+         * ----
+         *
+         * @author Milan Zamazal <mzamazal@redhat.com>
+         * @date 12 Dec 2016
+         * @status added
+         */
         @Out Cdrom cdrom();
     }
 }
