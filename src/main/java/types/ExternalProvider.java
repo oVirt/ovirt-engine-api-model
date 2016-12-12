07a685dbc47077178b96c36c14e5f5e8e2c54a9c
Mor Kalfon <mkalfon@redhat.com>
2016-12-12 17:21:52 +0200

    docstring types: OpenStack and ExternalProvider and services: OpenStackNetworkProviderService
    Change-Id: I2228e8874dffcf6e6e3c092293673462c6504c9b

--- src/main/java/types/ExternalProvider.java
+++ src/main/java/types/ExternalProvider.java
@@ -18,12 +18,68 @@ package types;
 
 import org.ovirt.api.metamodel.annotations.Type;
 
+/**
+ * Represents an external provider.
+ *
+ * @author Mor Kalfon <mkalfon@redhat.com>
+ * @date 12 Dec 2016
+ * @status added
+ */
 @Type
 public interface ExternalProvider extends Identified {
+    /**
+     * Defines URL address of the external provider.
+     *
+     * @author Mor Kalfon <mkalfon@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     String url();
+
+    /**
+     * Defines whether provider authentication is required or not.
+     *
+     * If authentication is required, both `username` and `password` attributes will be used during authentication.
+     *
+     * @author Mor Kalfon <mkalfon@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     Boolean requiresAuthentication();
+
+    /**
+     * Defines user name to be used during authentication process.
+     *
+     * @author Mor Kalfon <mkalfon@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     String username();
+
+    /**
+     * Defines password for the user during the authentication process.
+     *
+     * @author Mor Kalfon <mkalfon@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     String password();
+
+    /**
+     * Defines the external provider authentication URL address.
+     *
+     * @author Mor Kalfon <mkalfon@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     String authenticationUrl();
+
+    /**
+     * Array of provider name/value properties.
+     *
+     * @author Mor Kalfon <mkalfon@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     Property[] properties();
 }
