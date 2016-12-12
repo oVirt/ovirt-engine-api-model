4fa6527e442f4092fe85cacd96557d2d0720b6b1
Arik Hadas <ahadas@redhat.com>
2016-12-12 12:39:00 +0200

    Template type doc
    Change-Id: I2c3111605bf9c64415890d653d0673b9c9051b95
    Signed-off-by: Arik Hadas <ahadas@redhat.com>
    (cherry picked from commit 0552f832d38d0cb004ea776c29287f06f12da685)

--- src/main/java/types/Template.java
+++ src/main/java/types/Template.java
@@ -19,17 +19,95 @@ package types;
 import org.ovirt.api.metamodel.annotations.Link;
 import org.ovirt.api.metamodel.annotations.Type;
 
+/**
+ * Type representing a virtual machine template.
+ * This allows a rapid instanstiation of virtual machines with common configuration and disk states.
+ *
+ * @author Arik Hadas <ahadas@redhat.com>
+ * @date 12 Dec 2016
+ * @status added
+ */
 @Type
 public interface Template extends VmBase {
+    /**
+     * The status of the template.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     TemplateStatus status();
+
+    /**
+     * The virtual machine configuration associated with this template.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     Vm vm();
+
+    /**
+     * Indicates whether this is a base version or a sub version of another template.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     TemplateVersion version();
 
+    /**
+     * References to the CD-ROM devices attached to the template.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Link Cdrom[] cdroms();
+
+    /**
+     * References to the graphic consoles attached to the template.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Link GraphicsConsole[] graphicsConsoles();
+
+    /**
+     * References to the network interfaces attached to the template.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Link Nic[] nics();
+
+    /**
+     * References to the user permissions attached to the template.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Link Permission[] permissions();
+
+    /**
+     * References to the tags attached to the template.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Link Tag[] tags();
+
+    /**
+     * References to the watchdog devices attached to the template.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 12 Dec 2016
+     * @status added
+     */
     @Link Watchdog[] watchdogs();
 
     /**
