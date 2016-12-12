9e9e9ed2c8c1386fb7ef413b4985699900fab4b0
Ravi Nori <rnori@redhat.com>
2016-12-12 12:00:15 -0500

    Add documentation to DomainUserService
    Change-Id: I6c1a8c44090dab4fdc87d0c2eb45a8372e025923
    Signed-off-by: Ravi Nori <rnori@redhat.com>

--- src/main/java/services/aaa/DomainUserService.java
+++ src/main/java/services/aaa/DomainUserService.java
@@ -21,10 +21,53 @@ import org.ovirt.api.metamodel.annotations.Out;
 import org.ovirt.api.metamodel.annotations.Service;
 import types.User;
 
+/**
+ * A service to view a domain user in the system.
+ *
+ * @author Ravi Nori <rnori@redhat.com>
+ * @date 12 Dec 2016
+ * @status added
+ */
 @Service
 @Area("Infrastructure")
 public interface DomainUserService {
+    /**
+     * Gets the domain user information.
+     *
+     * Usage:
+     *
+     * ....
+     * GET /ovirt-engine/api/domains/5678/users/1234
+     * ....
+     *
+     * Will return the domain user information:
+     *
+     * [source,xml]
+     * ----
+     * <user href="/ovirt-engine/api/users/1234" id="1234">
+     *   <name>admin</name>
+     *   <namespace>*</namespace>
+     *   <principal>admin</principal>
+     *   <user_name>admin@internal-authz</user_name>
+     *   <domain href="/ovirt-engine/api/domains/5678" id="5678">
+     *     <name>internal-authz</name>
+     *   </domain>
+     *   <groups/>
+     * </user>
+     * ----
+     *
+     * @author Ravi Nori <rnori@redhat.com>
+     * @date 12 Dec 2017
+     * @status added
+     */
     interface Get {
+        /**
+         * The domain user.
+         *
+         * @author Ravi Nori <rnori@redhat.com>
+         * @date 12 Dec 2017
+         * @status added
+         */
         @Out User user();
     }
 }
