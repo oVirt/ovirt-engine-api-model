2e9dc9fd2e599ea0c1c65b5577e5acb6454c2aa2
Petr Horáček <phoracek@redhat.com>
2017-01-04 14:59:05 +0100

    deploy built documentation on github pages
    Change-Id: I5007c3fca63c7b7da86bee1482819779f01a81ab
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

---
+++ automation/deploy-on-gh-pages.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+#
+# Should be executed only by Travis CI which has encrypted keys available.
+
+
+set -xe
+
+
+function _load_ssh_key {
+    set +x  # hide keys
+    openssl aes-256-cbc \
+        -K $encrypted_3716b7dfd644_key \
+        -iv $encrypted_3716b7dfd644_iv \
+        -in automation/travis_rsa.enc \
+        -out automation/travis_rsa \
+        -d
+    set -x
+    eval "$(ssh-agent)"
+    chmod 0600 .travis/travis_rsa
+    ssh-add .travis/travis_rsa
+}
+
+
+function _init_git_config {
+    git config --global user.email "ovirt-engine-api-model@travis-ci.org"
+    git config --global user.name "oVirt Engine API Model"
+}
+
+
+function _clone_gh_pages {
+    git clone git@github.com:oVirt/ovirt-engine-api-model.git gh-pages
+    pushd gh-pages
+    git checkout origin/gh-pages -b gh-pages
+    popd
+}
+
+
+function _copy_to_master {
+    mkdir -p gh-pages/master
+    cp target/generated-html/model.html gh-pages/master/index.html
+}
+
+
+function _copy_to_tagged {
+    description=$(git describe)
+    version=${description:0:3}
+
+    mkdir -p gh-pages/${version}
+    cp target/generated-html/model.html gh-pages/${version}/index.html
+}
+
+
+function _push_gh_pages {
+    commit=$(git log --format="%H" -n 1)
+    description=$(git describe)
+
+    pushd gh-pages
+    git add .
+    git commit -m "gh-pages ${description} ${commit}"
+    git push origin HEAD:gh-pages
+    popd
+}
+
+
+_load_ssh_key
+_init_git_config
+_clone_gh_pages
+case "$1" in
+    master)
+        _copy_to_master
+        ;;
+    tagged)
+        _copy_to_tagged
+        ;;
+esac
+_push_gh_pages
