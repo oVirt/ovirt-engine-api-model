205f5156449f7aa950f9378b0e2500c175204a00
Martin Mucha <mmucha@redhat.com>
2017-01-20 11:20:01 +0100

    restapi: Added DEFAULT_ROUTE enum value to NetworkUsage enum.
    Change-Id: I05380c63f7d1cdac94867253551478f73c3022d5
    Signed-off-by: Martin Mucha <mmucha@redhat.com>

--- src/main/java/types/NetworkUsage.java
+++ src/main/java/types/NetworkUsage.java
@@ -20,6 +20,25 @@ import org.ovirt.api.metamodel.annotations.Type;
 
 @Type
 public enum NetworkUsage {
+    /**
+     * The default gateway and the DNS resolver configuration of the host will be taken from this network.
+     *
+     * If this network is attached to the host, then the DNS resolver configuration will be taken from the
+     * `dns_resolver_configuration` attribute of the network attachment. If there is no `dns_resolver_configuration`
+     * attribute in this network attachment, then they will be taken from the `dns_resolver_configuration` of the
+     * network itself. If `dns_resolver_configuration` attribute isn't present even there, DNS resolver configuration
+     * won't be set.
+     *
+     * If you set this flag on a network, then the the default gateway for the host will be taken from the `gateway`
+     * attribute of the `ip_address_assignment` of the network attachment.
+     *
+     * @author Martin Mucha <mmucha@redhat.com>
+     * @date 24 Jan 2017
+     * @status added
+     * @since 4.2
+     */
+    DEFAULT_ROUTE,
+
     DISPLAY,
 
     /**
