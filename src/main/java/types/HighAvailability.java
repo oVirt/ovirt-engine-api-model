097e48b529571f8d19c93cb9ac5daf126686236a
Juan Hernandez <juan.hernandez@redhat.com>
2017-03-23 10:15:23 +0100

    Improve HighAvailability.priority documentation
    Change-Id: Ic4a0989a20a95b670d8e622df669667f5bf279be
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>
    (cherry picked from commit 1101c49e82070d4f744c47b00c847e992c39fbf2)

--- src/main/java/types/HighAvailability.java
+++ src/main/java/types/HighAvailability.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2015 Red Hat, Inc.
+Copyright (c) 2017 Red Hat, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -21,5 +21,43 @@ import org.ovirt.api.metamodel.annotations.Type;
 @Type
 public interface HighAvailability {
     Boolean enabled();
+
+    /**
+     * Indicates the priority of the virtual machine inside the run and migration queues.
+     *
+     * Virtual machines with higher priorities will be started and migrated before virtual machines with lower
+     * priorities.
+     *
+     * The value is an integer between 0 and 100. The higher the value, the higher the priority.
+     *
+     * The graphical user interface (GUI) does not allow specifying all the possible values, instead
+     * it only allows you to select _Low_, _Medium_ or _High_. When the value is set using the API, the GUI
+     * will set the label as follows:
+     *
+     * |===
+     * |API Value |GUI Label
+     *
+     * |0 - 25 |Low
+     * |26 - 74 |Medium
+     * |75 - 100 |High
+     *
+     * |===
+     *
+     * When the label is selected using the GUI, the value in the API will be set as follows:
+     *
+     * |===
+     * |GUI Label |API Value
+     *
+     * |Low |1
+     * |Medium |50
+     * |High |100
+     *
+     * |===
+     *
+     * @author Juan Hernandez <juan.hernandez@redhat.com>
+     * @author Byron Graveorst <bgraveno@redhat.com>
+     * @date 31 Mar 2017
+     * @status updated_by_docs
+     */
     Integer priority();
 }
