0b98d2c18782a141233522010dfe94e58abce5ed
Oved Ourfali <oourfali@redhat.com>
2017-04-23 11:18:59 +0300

    documenting external status type
    Change-Id: I9e84fca65fba6d30625d63a9457519e7f76b3227
    Signed-off-by: Oved Ourfali <oourfali@redhat.com>
    (cherry picked from commit f3301ae19ecb644c5de42189cd8f43fa9a423f96)

--- src/main/java/types/ExternalStatus.java
+++ src/main/java/types/ExternalStatus.java
@@ -18,11 +18,65 @@ package types;
 
 import org.ovirt.api.metamodel.annotations.Type;
 
+/**
+ * Represents an external status.
+ * This status is currently used for <<types/host, hosts>>
+ * and <<types/storage_domain, storage domains>>, and allows an external
+ * system to update status of objects it is aware of.
+ *
+ * @author Oved Ourfali <oourfali@redhat.com>
+ * @date 23 Apr 2017
+ * @status added
+ */
 @Type
 public enum ExternalStatus {
+    /**
+     * Error status. There is some kind of error
+     * in the relevant object.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 23 Apr 2017
+     * @status added
+     */
     ERROR,
+
+    /**
+     * Failure status. The relevant object is failing.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 23 Apr 2017
+     * @status added
+     */
     FAILURE,
+
+    /**
+     * Info status. The relevant object is in OK status,
+     * but there is an information available that might be
+     * relevant for the administrator.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 23 Apr 2017
+     * @status added
+     */
     INFO,
+
+    /**
+     * OK status. The relevant object is working well.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 23 Apr 2017
+     * @status added
+     */
     OK,
+
+    /**
+     * Warning status. The relevant object is working well,
+     * but there is some warning that might be relevant for
+     * the administrator.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 23 Apr 2017
+     * @status added
+     */
     WARNING;
 }
