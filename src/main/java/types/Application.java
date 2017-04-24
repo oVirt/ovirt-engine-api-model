41581aa7b857c87084af60f61ee1ae88d90399f5
Lukas Svaty <lsvaty@redhat.com>
2017-04-24 08:44:31 +0200

    Application type doc
    Change-Id: I769e0c40829af4bab81ae2d10bb9f0b18b9f86a6
    Signed-off-by: Lukas Svaty <lsvaty@redhat.com>
    (cherry picked from commit 11646b958629d6d7ad73cfdc8acf4d4aa8ebf392)

--- src/main/java/types/Application.java
+++ src/main/java/types/Application.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2015 Red Hat, Inc.
+Copyright (c) 2015-2017 Red Hat, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -20,10 +20,26 @@ import org.ovirt.api.metamodel.annotations.Link;
 import org.ovirt.api.metamodel.annotations.Type;
 
 /**
- * Represents an application installed on a virtual machine.
- * Applications are reported by the guest agent, if you deploy
- * one on the virtual machine operating system.
+ * Represents an application installed on a virtual machine. Applications are reported by the guest agent, if you
+ * deploy one on the virtual machine operating system.
  *
+ * To get that information send a request like this:
+ *
+ * ....
+ * GET /ovirt-engine/api/vms/123/applications/456
+ * ....
+ *
+ * The result will be like this:
+ *
+ * [source,xml]
+ * ----
+ * <application href="/ovirt-engine/api/vms/123/applications/456" id="456">
+ *   <name>application-test-1.0.0-0.el7</name>
+ *   <vm href="/ovirt-engine/api/vms/123" id="123"/>
+ * </application>
+ * ----
+ *
+ * @author Lukas Svaty <lsvaty@redhat.com>
  * @author Oved Ourfali <oourfali@redhat.com>
  * @date 24 Apr 2017
  * @status added
@@ -31,8 +47,7 @@ import org.ovirt.api.metamodel.annotations.Type;
 @Type
 public interface Application extends Identified {
     /**
-     * A reference to the virtual machine
-     * the application is installed on.
+     * A reference to the virtual machine the application is installed on.
      *
      * @author Oved Ourfali <oourfali@redhat.com>
      * @date 24 Apr 2017
