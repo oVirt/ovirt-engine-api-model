e271304bb19daa73fafbb77dff56dce6aea93954
Yaniv Bronhaim <ybronhei@redhat.com>
2017-04-24 15:09:52 +0300

    Adding docs about GET hostprovider API
    Change-Id: I971f2c244d15c88bdb7018e3aad8fb3cb1c47372
    Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>

--- src/main/java/services/externalhostproviders/ExternalHostProviderService.java
+++ src/main/java/services/externalhostproviders/ExternalHostProviderService.java
@@ -26,9 +26,47 @@ import types.ExternalHostProvider;
 
 import static org.ovirt.api.metamodel.language.ApiLanguage.optional;
 
+/**
+ * Represents an external host provider, such as Foreman or Satellite.
+ *
+ * See https://www.theforeman.org/ for more details on Foreman.
+ * See https://access.redhat.com/products/red-hat-satellite for more details on Red Hat Satellite.
+ *
+ * @author Yaniv Bronhaim <ybronhei@redhat.com>
+ * @date 24 Apr 2017
+ * @status added
+ */
 @Service
 @Area("Infrastructure")
 public interface ExternalHostProviderService extends ExternalProviderService {
+    /**
+     * Get external host provider information
+     *
+     * Host provider, Foreman or Satellite, can be set as an external provider in ovirt. To see details about specific
+     * host providers attached to ovirt use this API.
+     *
+     * For example, to get the details of host provider `123`, send a request like this:
+     *
+     * ....
+     * GET /ovirt-engine/api/externalhostproviders/123
+     * ....
+     *
+     * The response will be like this:
+     *
+     * [source,xml]
+     * ----
+     * <external_host_provider href="/ovirt-engine/api/externalhostproviders/123" id="123">
+     *   <name>mysatellite</name>
+     *   <requires_authentication>true</requires_authentication>
+     *   <url>https://mysatellite.example.com</url>
+     *   <username>admin</username>
+     * </external_host_provider>
+     * ----
+     *
+     * @author Yaniv Bronhaim <ybronhei@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     interface Get {
         @Out ExternalHostProvider provider();
     }
