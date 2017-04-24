49ab340d28c245ed9fd94f678e39c5ae1a830867
Arik Hadas <ahadas@redhat.com>
2017-04-24 10:29:12 +0300

    VmPoolType type doc
    Change-Id: Iee6b3e945cb2a19ce617db5732fb5770b2cc3591
    Signed-off-by: Arik Hadas <ahadas@redhat.com>
    (cherry picked from commit 2e1f20150984d50b676d4274aa54230944365227)

--- src/main/java/types/VmPoolType.java
+++ src/main/java/types/VmPoolType.java
@@ -17,8 +17,38 @@ package types;
 
 import org.ovirt.api.metamodel.annotations.Type;
 
+/**
+ * Type represeting the deallocation policy of virtual machines in a virtual machines pool.
+ *
+ * @author Arik Hadas <ahadas@redhat.com>
+ * @date 24 Apr 2017
+ * @status added
+ */
 @Type
 public enum VmPoolType {
+    /**
+     * This policy indicates that virtual machines in the pool are automcatically deallocated by the system.
+     *
+     * With this policy, when a virtual machine that is part of the pool and is assigned to a user is shut-down, it is
+     * detached from the user, its state is restored to the pool's default state, and the virtual machine returns to
+     * pool (i.e., the virtual machine can then be assigned to another user).
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     AUTOMATIC,
+
+    /**
+     * This policy indicates that virtual machines in the pool are deallocated manually by the administrator.
+     *
+     * With this policy, a virtual machine that is part of the pool remains assigned to its user and preserves its state
+     * on shut-down. In order to return the virtual machine back to the pool, the administrator needs to deallocate it
+     * explicitly by removing the user's permissions on that virtual machine.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     MANUAL;
 }
