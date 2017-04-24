e21553a2b606ab9b4a27f25bfd16ceecb3e519df
StLuke <svatylukas@gmail.com>
2017-04-24 11:20:07 +0200

    Adding doc for guest operating system type
    Change-Id: I9d6c88b39918a4639499d8f1323efff1a0d6d96d
    Signed-off-by: StLuke <svatylukas@gmail.com>
    (cherry picked from commit e17f05c014542d90f6e85c56049dfe817ffb9255)

--- src/main/java/types/GuestOperatingSystem.java
+++ src/main/java/types/GuestOperatingSystem.java
@@ -18,12 +18,101 @@ package types;
 
 import org.ovirt.api.metamodel.annotations.Type;
 
+/**
+ * Represents an operating system installed on the virtual machine.
+ *
+ * To get that information send a request like this:
+ *
+ * ....
+ * GET /ovirt-engine/api/vms/123
+ * ....
+ *
+ * The result will be like this:
+ *
+ * [source,xml]
+ * ----
+ * <vm href="/ovirt-engine/api/vms/123" id="123">
+ * ...
+ *   <guest_operating_system>
+ *     <architecture>x86_64</architecture>
+ *     <codename>Maipo</codename>
+ *     <distribution>Red Hat Enterprise Linux Server</distribution>
+ *     <family>Linux</family>
+ *     <kernel>
+ *       <version>
+ *         <build>0</build>
+ *         <full_version>3.10.0-514.10.2.el7.x86_64</full_version>
+ *         <major>3</major>
+ *         <minor>10</minor>
+ *         <revision>514</revision>
+ *       </version>
+ *     </kernel>
+ *     <version>
+ *       <full_version>7.3</full_version>
+ *       <major>7</major>
+ *       <minor>3</minor>
+ *     </version>
+ *   </guest_operating_system>
+ * </vm>
+ * ----
+ *
+ * @author Lukas Svaty <lsvaty@redhat.com>
+ * @date 24 Apr 2017
+ * @status added
+ */
 @Type
 public interface GuestOperatingSystem {
+    /**
+     * The architecture of the operating system, such as x86_64.
+     *
+     * @author Lukas Svaty <lsvaty@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     String architecture();
+
+    /**
+     * Code name of the operating system, such as `Maipo`.
+     *
+     * @author Lukas Svaty <lsvaty@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     String codename();
+
+    /**
+     * Full name of operating system distribution.
+     *
+     * @author Lukas Svaty <lsvaty@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     String distribution();
+
+    /**
+     * Kernel version of the operating system.
+     *
+     * @author Lukas Svaty <lsvaty@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     Kernel kernel();
+
+    /**
+     * Family of operating system, such as `Linux`.
+     *
+     * @author Lukas Svaty <lsvaty@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     String family();
+
+    /**
+     * Version of the installed operating system.
+     *
+     * @author Lukas Svaty <lsvaty@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     Version version();
 }
