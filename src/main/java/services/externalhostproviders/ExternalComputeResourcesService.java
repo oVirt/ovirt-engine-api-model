d81981252aea116b16b6e63ae7098b521078a010
Yaniv Bronhaim <ybronhei@redhat.com>
2017-04-24 13:58:21 +0300

    Adding docs for external compute resources API
    Change-Id: Ib2a59328d65a3cd1015b2eec17f12a6368ac93e2
    Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>

--- src/main/java/services/externalhostproviders/ExternalComputeResourcesService.java
+++ src/main/java/services/externalhostproviders/ExternalComputeResourcesService.java
@@ -22,10 +22,57 @@ import org.ovirt.api.metamodel.annotations.Out;
 import org.ovirt.api.metamodel.annotations.Service;
 import types.ExternalComputeResource;
 
+/**
+ * Manages a collection of external compute resources.
+ *
+ * Compute resource is a term of host external provider. The external provider also needs to know to where the
+ * provisioned host needs to register. The login details of the engine is saved as a compute resource in the external
+ * provider side.
+ *
+ * @author Yaniv Bronhaim <ybronhei@redhat.com>
+ * @date 24 Apr 2017
+ * @status added
+ */
 @Service
 @Area("Infrastructure")
 public interface ExternalComputeResourcesService {
+    /**
+     * Retrieves a list of external compute resources.
+     *
+     * For example, to retrieve the compute resources of external host provider `123`, send a request like this:
+     *
+     * ....
+     * GET /ovirt-engine/api/externalhostproviders/123/computeresources
+     * ....
+     *
+     * It will return a response like this:
+     *
+     * [source,xml]
+     * ----
+     * <external_compute_resources>
+     *   <external_compute_resource href="/ovirt-engine/api/externalhostproviders/123/computeresources/234" id="234">
+     *     <name>hostname</name>
+     *     <provider>oVirt</provider>
+     *     <url>https://address/api</url>
+     *     <user>admin@internal</user>
+     *     <external_host_provider href="/ovirt-engine/api/externalhostproviders/123" id="123"/>
+     *    </external_compute_resource>
+     *    ...
+     * </external_compute_resources>
+     * ----
+     *
+     * @author Yaniv Bronhaim <ybronhei@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     interface List {
+        /**
+         * List of external computer resources.
+         *
+         * @author Yaniv Bronhaim <ybronhei@redhat.com>
+         * @date 24 Apr 2017
+         * @status added
+         */
         @Out ExternalComputeResource[] resources();
 
         /**
@@ -34,5 +81,12 @@ public interface ExternalComputeResourcesService {
         @In Integer max();
     }
 
+    /**
+     * This service manages compute resource instance
+     *
+     * @author Yaniv Bronhaim <ybronhei@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     @Service ExternalComputeResourceService resource(String id);
 }
