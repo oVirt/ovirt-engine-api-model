b11756d2af59991bb11a19b8e24f14d7b7e51230
Oved Ourfali <oourfali@redhat.com>
2017-04-24 13:17:25 +0300

    documenting external host provider
    Change-Id: Ib9238feb83d157f99c21779874492804af56a14a
    Signed-off-by: Oved Ourfali <oourfali@redhat.com>
    (cherry picked from commit 52f0f2b66e133696a6420c18a638753c734fc72b)

--- src/main/java/types/ExternalHostProvider.java
+++ src/main/java/types/ExternalHostProvider.java
@@ -19,11 +19,72 @@ package types;
 import org.ovirt.api.metamodel.annotations.Link;
 import org.ovirt.api.metamodel.annotations.Type;
 
+/**
+ * Represents an external host provider,
+ * such as Foreman or Satellite.
+ *
+ * See https://www.theforeman.org/ for more details on Foreman.
+ * See https://access.redhat.com/products/red-hat-satellite
+ * for more details on Red Hat Satellite.
+ *
+ * @author Oved Ourfali <oourfali@redhat.com>
+ * @date 24 Apr 2017
+ * @status added
+ */
 @Type
 public interface ExternalHostProvider extends ExternalProvider {
+
+    /**
+     * A reference to the certificates the engine supports
+     * for this provider.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     @Link Certificate[] certificates();
+
+    /**
+     * A reference to the compute resource as represented in the host provider.
+     * Each host provider optionally has the engine defined as a compute resource,
+     * which allows to create virtual machines in the engine. This compute resource
+     * details are used in the Bare-Metal provisioning use-case, in order to
+     * deploy the hypervisor.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     @Link ExternalComputeResource[] computeResources();
+
+    /**
+     * A reference to the discovered hosts in the host provider.
+     * Discovered hosts are hosts that were not provisioned yet.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     @Link ExternalDiscoveredHost[] discoveredHosts();
+
+    /**
+     * A reference to the host groups in the host provider.
+     * Host group contains different properties that the host
+     * provider applies on all hosts that are member of this group.
+     * Such as installed software, system definitions, passwords and more.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     @Link ExternalHostGroup[] hostGroups();
+
+    /**
+     * A reference to the hosts provisioned by the host provider.
+     *
+     * @author Oved Ourfali <oourfali@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     @Link Host[] hosts();
 }
