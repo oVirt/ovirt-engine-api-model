f44e138c57862c806380e9a2c331a93dace51ce0
Yaniv Bronhaim <ybronhei@redhat.com>
2017-04-24 13:15:43 +0300

    Adding service docs to External hosts provider API
    Change-Id: I4760e355bb30a7b746754ce8d74ba0608f8e6d93
    Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>

--- src/main/java/services/externalhostproviders/ExternalDiscoveredHostsService.java
+++ src/main/java/services/externalhostproviders/ExternalDiscoveredHostsService.java
@@ -22,10 +22,59 @@ import org.ovirt.api.metamodel.annotations.Out;
 import org.ovirt.api.metamodel.annotations.Service;
 import types.ExternalDiscoveredHost;
 
+/**
+ * This service manages external discovered hosts.
+ *
+ * @author Yaniv Bronhaim <ybronhei@redhat.com>
+ * @date 24 Apr 2017
+ * @status added
+ */
 @Service
 @Area("Infrastructure")
 public interface ExternalDiscoveredHostsService {
+    /**
+     * Get list of discovered hosts' information.
+     *
+     * Discovered hosts are fetched from third-party providers such as Foreman.
+     *
+     * To list all discovered hosts for provider `123` send the following:
+     *
+     * [source]
+     * ----
+     * GET /ovirt-engine/api/externalhostproviders/123/discoveredhost
+     * ----
+     *
+     * [source,xml]
+     * ----
+     * <external_discovered_hosts>
+     *  <external_discovered_host href="/ovirt-engine/api/externalhostproviders/123/discoveredhosts/456" id="456">
+     *   <name>mac001a4ad04031</name>
+     *   <ip>10.34.67.42</ip>
+     *   <last_report>2017-04-24 11:05:41 UTC</last_report>
+     *   <mac>00:1a:4a:d0:40:31</mac>
+     *   <subnet_name>sat0</subnet_name>
+     *   <external_host_provider href="/ovirt-engine/api/externalhostproviders/123" id="123"/>
+     *  </external_discovered_host>
+     *  <external_discovered_host href="/ovirt-engine/api/externalhostproviders/123/discoveredhosts/789" id="789">
+     *   <name>mac001a4ad04040</name>
+     *   <ip>10.34.67.43</ip>
+     *   <last_report>2017-04-24 11:05:41 UTC</last_report>
+     *   <mac>00:1a:4a:d0:40:40</mac>
+     *   <subnet_name>sat0</subnet_name>
+     *   <external_host_provider href="/ovirt-engine/api/externalhostproviders/123" id="123"/>
+     *  </external_discovered_host>
+     *  ...
+     * </external_discovered_hosts>
+     * ----
+     *
+     * @author Yaniv Bronhaim <ybronhei@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     interface List {
+        /**
+         * List of discovered hosts
+         */
         @Out ExternalDiscoveredHost[] hosts();
 
         /**
