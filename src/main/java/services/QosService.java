d966552b42f1317a6b29f5f8b146dabbe5ec0ad4
Aleksei Slaikovskii <aslaikov@redhat.com>
2017-04-24 13:28:41 +0200

    Added missing docstrings for QoS service.
    Change-Id: I480f97f578ab0004ae4e1efcb2417c85cd1a3a9e
    Signed-off-by: Aleksei Slaikovskii <aslaikov@redhat.com>
    (cherry picked from commit 85115f97317eff076ff426582572d04f0b200910)

--- src/main/java/services/QosService.java
+++ src/main/java/services/QosService.java
@@ -25,11 +25,87 @@ import types.Qos;
 @Service
 @Area("SLA")
 public interface QosService {
+    /**
+     * Get specified QoS in the data center.
+     *
+     * [source]
+     * ----
+     * GET /ovirt-engine/api/datacenters/123/qoss/123
+     * ----
+     *
+     * You will get response like this one below:
+     *
+     * [source,xml]
+     * ----
+     * <qos href="/ovirt-engine/api/datacenters/123/qoss/123" id="123">
+     *   <name>123</name>
+     *   <description>123</description>
+     *   <max_iops>1</max_iops>
+     *   <max_throughput>1</max_throughput>
+     *   <type>storage</type>
+     *   <data_center href="/ovirt-engine/api/datacenters/123" id="123"/>
+     * </qos>
+     * ----
+     *
+     * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     interface Get {
+        /**
+         * Queried QoS object.
+         *
+         * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+         * @date 24 Apr 2017
+         * @status added
+         */
         @Out Qos qos();
     }
 
+    /**
+     * Update the specified QoS in the dataCenter.
+     *
+     * [source]
+     * ----
+     * PUT /ovirt-engine/api/datacenters/123/qoss/123
+     * ----
+     *
+     * For example with curl:
+     *
+     * [source]
+     * ----
+     * curl -u admin@internal:123456 -X PUT -H "content-type: application/xml" -d \
+     * "<qos><name>321</name><description>321</description><max_iops>10</max_iops></qos>" \
+     * https://engine/ovirt-engine/api/datacenters/123/qoss/123
+     * ----
+     *
+     * You will receive response like this:
+     *
+     * [source,xml]
+     * ----
+     * <qos href="/ovirt-engine/api/datacenters/123/qoss/123" id="123">
+     *   <name>321</name>
+     *   <description>321</description>
+     *   <max_iops>10</max_iops>
+     *   <max_throughput>1</max_throughput>
+     *   <type>storage</type>
+     *   <data_center href="/ovirt-engine/api/datacenters/123" id="123"/>
+     * </qos>
+     * ----
+     *
+     * @author Ori Liel <oliel@redhat.com>
+     * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+     * @date 26 Apr 2017
+     * @status updated
+     */
     interface Update {
+        /**
+         * Updated QoS object.
+         *
+         * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+         * @date 24 Apr 2017
+         * @status added
+         */
         @In @Out Qos qos();
 
         /**
@@ -38,6 +114,18 @@ public interface QosService {
         @In Boolean async();
     }
 
+    /**
+     * Remove specified QoS from datacenter.
+     *
+     * [source]
+     * ----
+     * DELETE /ovirt-engine/api/datacenters/123/qoss/123
+     * ----
+     *
+     * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     interface Remove {
         /**
          * Indicates if the remove should be performed asynchronously.
