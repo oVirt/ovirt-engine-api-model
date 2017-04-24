0f7594ecbc9a0765acc150dbfa0816cc006a86f6
Yaniv Bronhaim <ybronhei@redhat.com>
2017-04-24 15:03:58 +0300

    Adding docs for hostgroups service
    Change-Id: I2c3fb7035d201d37fd4b9076e6dec2d56910bd8d
    Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>

--- src/main/java/services/externalhostproviders/ExternalHostGroupsService.java
+++ src/main/java/services/externalhostproviders/ExternalHostGroupsService.java
@@ -22,10 +22,57 @@ import org.ovirt.api.metamodel.annotations.Out;
 import org.ovirt.api.metamodel.annotations.Service;
 import types.ExternalHostGroup;
 
+/**
+ * This service manages hostgroups.
+ *
+ * @author Yaniv Bronhaim <ybronhei@redhat.com>
+ * @date 24 Apr 2017
+ * @status added
+ */
 @Service
 @Area("Infrastructure")
 public interface ExternalHostGroupsService {
+    /**
+     * Get hostgroups list from external host provider.
+     *
+     * Host group is a term of host providers - the host group includes provision details. This API returns all possible
+     * hostgroups exposed by the external provider.
+     *
+     * For example, to get the details of all host groups of provider `123`, send a request like this:
+     *
+     * ....
+     * GET /ovirt-engine/api/externalhostproviders/123/hostgroups
+     * ....
+     *
+     * The response will be like this:
+     *
+     * [source,xml]
+     * ----
+     * <external_host_groups>
+     *   <external_host_group href="/ovirt-engine/api/externalhostproviders/123/hostgroups/234" id="234">
+     *     <name>rhel7</name>
+     *     <architecture_name>x86_64</architecture_name>
+     *     <domain_name>example.com</domain_name>
+     *     <operating_system_name>RedHat 7.3</operating_system_name>
+     *     <subnet_name>sat0</subnet_name>
+     *     <external_host_provider href="/ovirt-engine/api/externalhostproviders/123" id="123"/>
+     *   </external_host_group>
+     *   ...
+     * </external_host_groups>
+     * ----
+     *
+     * @author Yaniv Bronhaim <ybronhei@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     interface List {
+        /**
+         * List of all hostgroups available for external host provider
+         *
+         * @author Yaniv Bronhaim <ybronhei@redhat.com>
+         * @date 24 Apr 2017
+         * @status added
+         */
         @Out ExternalHostGroup[] groups();
 
         /**
@@ -34,5 +81,12 @@ public interface ExternalHostGroupsService {
         @In Integer max();
     }
 
+    /**
+     * This service manages hostgroup instance.
+     *
+     * @author Yaniv Bronhaim <ybronhei@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     @Service ExternalHostGroupService group(String id);
 }
