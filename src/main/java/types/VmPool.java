39ac74fb0b00b0f87fa137ee4402795e993b82dc
Arik Hadas <ahadas@redhat.com>
2017-04-24 18:17:24 +0300

    VmPool type doc
    Change-Id: I752c7f2d6bf401fb98976379da12e372ab234c34
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- src/main/java/types/VmPool.java
+++ src/main/java/types/VmPool.java
@@ -19,23 +19,96 @@ package types;
 import org.ovirt.api.metamodel.annotations.Link;
 import org.ovirt.api.metamodel.annotations.Type;
 
+/**
+ * Type represeting a virtual machines pool.
+ *
+ * @author Arik Hadas <ahadas@redhat.com>
+ * @date 24 Apr 2017
+ * @status added
+ */
 @Type
 public interface VmPool extends Identified {
+    /**
+     * The number of virtual machines in the pool.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     Integer size();
+
+    /**
+     * The system attempts to prestart the specified number of virtual machines from the pool.
+     *
+     * These virtual machines are started without being attached to any user. That way, users can acquire virtual
+     * machines from the pool faster.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     Integer prestartedVms();
+
+    /**
+     * The maximum number of virtual machines in the pool that could be assigned to a particular user.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     Integer maxUserVms();
+
+    /**
+     * The display settings configured for virtual machines in the pool.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     Display display();
+
+    /**
+     * The random number generator device configured for virtual machines in the pool.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     RngDevice rngDevice();
+
+    /**
+     * Indicates if sound card should be configured for virtual machines in the pool.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     Boolean soundcardEnabled();
+
+    /**
+     * The deallocation policy of virtual machines in the pool.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     VmPoolType type();
+
+    /**
+     * Indicates if virtual machines in the pool are updated to newer versions of the template the pool is based on.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     Boolean useLatestTemplateVersion();
 
     /**
      * Virtual machine pool's stateful flag.
      *
-     * Virtual machines from a stateful virtual machine pool are always started in stateful mode
-     * (stateless snapshot is not created). The state of the virtual machine is preserved
-     * even when the virtual machine is passed to a different user.
+     * Virtual machines from a stateful virtual machine pool are always started in stateful mode (stateless snapshot is
+     * not created). The state of the virtual machine is preserved even when the virtual machine is passed to a
+     * different user.
      *
      * @author Byron Gravenorst <bgraveno@redhat.com>
      * @date 8 Dec 2016
@@ -60,7 +133,22 @@ public interface VmPool extends Identified {
      */
     Boolean autoStorageSelect();
 
+    /**
+     * Reference to the cluster the pool resides in.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     @Link Cluster cluster();
+
+    /**
+     * Reference to the template the pool is based on.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     @Link Template template();
 
     /**
@@ -72,6 +160,25 @@ public interface VmPool extends Identified {
      * @status updated_by_docs
      */
     @Link InstanceType instanceType();
+
+    /**
+     * Reference to an arbitrary virtual machine that is part of the pool.
+     *
+     * Note that this virtual machine may not be based to the latest version of
+     * the pool's template.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     @Link Vm vm();
+
+    /**
+     * Permissions set for this virtual machine pool.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     @Link Permission[] permissions();
 }
