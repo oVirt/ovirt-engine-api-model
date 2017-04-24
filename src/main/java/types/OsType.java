29b63779703526dc9680f6dd1b54246b06601296
Arik Hadas <ahadas@redhat.com>
2017-04-24 18:16:54 +0300

    OsType type doc
    Change-Id: Ie99f3047e4c7a609384eea2f40f3966dd35f3ea8
    Signed-off-by: Arik Hadas <ahadas@redhat.com>

--- src/main/java/types/OsType.java
+++ src/main/java/types/OsType.java
@@ -17,28 +17,216 @@ package types;
 
 import org.ovirt.api.metamodel.annotations.Type;
 
+/**
+ * Type representing kind of operating system.
+ *
+ * The end-user declares the type of the operating system installed in the virtual machine (guest operating system) by
+ * selecting one of these values. This declaration enables the system to tune the virtual machine configuration for
+ * better user experience. For example, the system chooses devices that are most suitable for the operating system. Note
+ * that the system rely on user's selection and does not verify it by inspecting the actual guest operating system
+ * installed.
+ *
+ * @author Arik Hadas <ahadas@redhat.com>
+ * @date 24 Apr 2017
+ * @status added
+ */
 @Type
 public enum OsType {
+    /**
+     * This value is mapped to `other`.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     UNASSIGNED,
+
+    /**
+     * Windows XP.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     WINDOWS_XP,
+
+    /**
+     * Windows 2003 32-bit.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     WINDOWS_2003,
+
+    /**
+     * Windows 2008 32-bit.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     WINDOWS_2008,
+
+    /**
+     * Distribution of Linux other than those specified by the other values.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     OTHER_LINUX,
+
+    /**
+     * Other type of operating system, not specified by the other values.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     OTHER,
+
+    /**
+     * Red Hat Enterprise Linux 5 32-bit.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     RHEL_5,
+
+    /**
+     * Red Hat Enterprise Linux 4 32-bit.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     RHEL_4,
+
+    /**
+     * Red Hat Enterprise Linux 3 32-bit.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     RHEL_3,
+
+    /**
+     * Windows 2003 64-bit.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     WINDOWS_2003X64,
+
+    /**
+     * Windows 7 32-bit.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     WINDOWS_7,
+
+    /**
+     * Windows 7 64-bit.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     WINDOWS_7X64,
+
+    /**
+     * Red Hat Enterprise Linux 5 64-bit.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     RHEL_5X64,
+
+    /**
+     * Red Hat Enterprise Linux 4 64-bit.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     RHEL_4X64,
+
+    /**
+     * Red Hat Enterprise Linux 3 64-bit.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     RHEL_3X64,
+
+    /**
+     * Windows 2008 64-bit.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     WINDOWS_2008X64,
+
+    /**
+     * Windows 2008 R2 64-bit.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     WINDOWS_2008R2X64,
+
+    /**
+     * Red Hat Enterprise Linux 6 32-bit.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     RHEL_6,
+
+    /**
+     * Red Hat Enterprise Linux 6 64-bit.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     RHEL_6X64,
+
+    /**
+     * Windows 8 32-bit.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     WINDOWS_8,
+
+    /**
+     * Windows 8 64-bit.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     WINDOWS_8X64,
+
+    /**
+     * Windows 2012 64-bit.
+     *
+     * @author Arik Hadas <ahadas@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     WINDOWS_2012X64;
 }
