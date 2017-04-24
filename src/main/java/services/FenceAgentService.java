05fd6cdd19bcf1b4e986eb2383d352b3c82e2dd8
Piotr Kliczewski <piotr.kliczewski@gmail.com>
2017-04-24 14:37:43 +0200

    Add documentation to FenceAgentService
    Change-Id: Ic55e629d832bb5f8827589b87a249aeefd0c4220
    Signed-off-by: Piotr Kliczewski <piotr.kliczewski@gmail.com>
    (cherry picked from commit 8c3efdb156da9be153ebe85da8ad8c8d1efc65e3)

--- src/main/java/services/FenceAgentService.java
+++ src/main/java/services/FenceAgentService.java
@@ -22,14 +22,66 @@ import org.ovirt.api.metamodel.annotations.Out;
 import org.ovirt.api.metamodel.annotations.Service;
 import types.Agent;
 
+/**
+ * A service to manage fence agent for a specific host.
+ *
+ * @author Piotr Kliczewski <pkliczew@redhat.com>
+ * @date 24 Apr 2017
+ * @status added
+ *
+ */
 @Service
 @Area("Infrastructure")
 public interface FenceAgentService {
+
+    /**
+     * Gets details of this fence agent.
+     *
+     * [source]
+     * ----
+     * GET /ovirt-engine/api/hosts/123/fenceagents/0
+     * ----
+     *
+     * And here is sample response:
+     *
+     * [source,xml]
+     * ----
+     * <agent id="0">
+     *   <type>apc</type>
+     *   <order>1</order>
+     *   <ip>192.168.1.101</ip>
+     *   <user>user</user>
+     *   <password>xxx</password>
+     *   <port>9</port>
+     *   <options>name1=value1, name2=value2</options>
+     * </agent>
+     * ----
+     *
+     * @author Piotr Kliczewski <pkliczew@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     *
+     */
     interface Get {
+
+        /**
+         * Fence agent details.
+         *
+         * @author Piotr Kliczewski <pkliczew@redhat.com>
+         * @date 24 Apr 2017
+         * @status added
+         */
         @Out Agent agent();
     }
 
     interface Update {
+        /**
+         * Fence agent details.
+         *
+         * @author Piotr Kliczewski <pkliczew@redhat.com>
+         * @date 24 Apr 2017
+         * @status added
+         */
         @In @Out Agent agent();
 
         /**
@@ -38,6 +90,18 @@ public interface FenceAgentService {
         @In Boolean async();
     }
 
+    /**
+     * Removes a fence agent for a specific host.
+     *
+     * [source]
+     * ----
+     * DELETE /ovirt-engine/api/hosts/123/fenceagents/0
+     * ----
+     *
+     * @author Piotr Kliczewski <pkliczew@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     interface Remove {
         /**
          * Indicates if the remove should be performed asynchronously.
