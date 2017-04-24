e10616c4b90378d3ab28a19c5a93a37009a506d3
Aleksei Slaikovskii <aslaikov@redhat.com>
2017-04-24 12:52:16 +0200

    Added missing docstring for StorageDomainType type.
    Change-Id: I309fae680fa25142a615425a63b77ae30aedbeff
    Signed-off-by: Aleksei Slaikovskii <aslaikov@redhat.com>

--- src/main/java/types/StorageDomainType.java
+++ src/main/java/types/StorageDomainType.java
@@ -17,11 +17,65 @@ package types;
 
 import org.ovirt.api.metamodel.annotations.Type;
 
+/**
+ * Indicates the kind of data managed by a <<types/storage_domain, storage domain>>.
+ *
+ * @author Aleksei Slaikovski <aslaikov@redhat.com>
+ * @date 24 Apr 2017
+ * @status added
+ */
 @Type
 public enum StorageDomainType {
+    /**
+     * Data domains are used to store the disks and snapshots of the virtual machines and templates in the system. In
+     * addition, snapshots of the disks are also stored in data domains. Data domains cannot be shared across data
+     * centers.
+     *
+     * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     DATA,
+
+    /**
+     * ISO domains store ISO files (or logical CDs) used to install and boot operating systems and applications for the
+     * virtual machines. ISO domains remove the data center's need for physical media. An ISO domain can be shared
+     * across different data centers.
+     *
+     * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     ISO,
+
+    /**
+     * Export domains are temporary storage repositories used to copy and move virtual machines and templates between
+     * data centers and {product-name} environments. Export domains can also be used to backup virtual machines. An
+     * export domain can be moved between data centers but it can only be active in one data center at a time.
+     *
+     * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     EXPORT,
+
+    /**
+     * Image domain store images that can be imported into from an external system. For example, images from an
+     * OpenStack Glance image repository.
+     *
+     * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     IMAGE,
+
+    /**
+     * Volume domains store logical volumes that can be used as disks for virtual machines. For example, volumes
+     * from an OpenStack Cincer block storage service.
+     *
+     * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     VOLUME;
 }
