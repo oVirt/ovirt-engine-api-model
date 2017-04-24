6312b11279defac9c333a64d8060761b616fe6f6
Aleksei Slaikovskii <aslaikov@redhat.com>
2017-04-24 12:57:48 +0200

    Added missing docstring for StorageFormat type.
    Change-Id: I95f6df767c17e875907eafcf3e09f20bc6a2f48a
    Signed-off-by: Aleksei Slaikovskii <aslaikov@redhat.com>
    (cherry picked from commit 403362eb807de58fd8ec4721e1724f2431728670)

--- src/main/java/types/StorageFormat.java
+++ src/main/java/types/StorageFormat.java
@@ -18,10 +18,54 @@ package types;
 
 import org.ovirt.api.metamodel.annotations.Type;
 
+/**
+ * Type which represents a format of <<types/storage_domain, storage domain>>.
+ *
+ * @author Aleksei Slaikovskii <aslaikov@redhat.com>
+ * @date 24 Apr 2017
+ * @status added
+ */
 @Type
 public enum StorageFormat {
+    /**
+     * Version 1 of the storage domain format is applicable to NFS, iSCSI and FC storage domains.
+     *
+     * Each storage domain contains metadata describing its own structure, and all of the names of physical volumes that
+     * are used to back virtual machine disk images. Master domains additionally contain metadata for all the domains
+     * and physical volume names in the storage pool. The total size of this metadata is limited to 2 KiB, limiting the
+     * number of storage domains that can be in a pool. Template and virtual machine base images are read only.
+     *
+     * @author Aleksei Slaikovsky <aslaikov@rehat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     V1,
+
+    /**
+     * Version 2 of the storage domain format is applicable to iSCSI and FC storage domains.
+     *
+     * All storage domain and pool metadata is stored as logical volume tags rather than written to a logical volume.
+     * Metadata about virtual machine disk volumes is still stored in a logical volume on the domains. Physical volume
+     * names are no longer included in the metadata. Template and virtual machine base images are read only.
+     *
+     * @author Aleksei Slaikovsky <aslaikov@rehat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     V2,
+
+    /**
+     * Version 3 of the storage domain format is applicable to NFS, POSIX, iSCSI and FC storage domains.
+     *
+     * All storage domain and pool metadata is stored as logical volume tags rather than written to a logical volume.
+     * Metadata about virtual machine disk volumes is still stored in a logical volume on the domains. Virtual machine
+     * and template base images are no longer read only. This change enables live snapshots, live storage migration,
+     * and clone from snapshot. Support for Unicode metadata is added, for non-English volume names.
+     *
+     * @author Aleksei Slaikovsky <aslaikov@rehat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     V3,
 
     /**
