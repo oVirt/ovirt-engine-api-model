df699ccbc3c10866b3bdbe933ff28e6ce6c4fe02
Shahar Havivi <shaharh@redhat.com>
2017-04-24 13:54:30 +0300

    Added documentation for DiskStatus
    Change-Id: Ifc9d3ac16a93875df3c6bc7dcad10693e897efef
    Signed-off-by: Shahar Havivi <shaharh@redhat.com>

--- src/main/java/types/DiskStatus.java
+++ src/main/java/types/DiskStatus.java
@@ -17,10 +17,43 @@ package types;
 
 import org.ovirt.api.metamodel.annotations.Type;
 
+/**
+ * Current status representation for disk.
+ *
+ * @author Shahar Havivi <shavivi@redhat.com>
+ * @date 24 Apr 2017
+ * @status added
+ */
 @Type
 public enum DiskStatus {
 
+    /**
+     * Disk cannot be accessed by the virtual machine, and the user needs
+     * to take action to resolve the issue.
+     *
+     * @author Shahar Havivi <shavivi@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     ILLEGAL,
+
+    /**
+     * The disk is being used by the system, therefore it cannot be accessed
+     * by virtual machines at this point. This is usually a temporary status,
+     * until the disk is freed.
+     *
+     * @author Shahar Havivi <shavivi@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     LOCKED,
+
+    /**
+     * The disk status is normal and can be accessed by the virtual machine.
+     *
+     * @author Shahar Havivi <shavivi@redhat.com>
+     * @date 24 Apr 2017
+     * @status added
+     */
     OK;
 }
