a7a616ac410a6b348d77cde49e4e9bcffe4c0180
Jakub Niedermertl <jniederm@redhat.com>
2017-04-25 04:23:33 +0200

    OperatingSystemInfo type doc
    Change-Id: I0876271cca978a2377a2805c892db7580a898651
    Signed-off-by: Jakub Niedermertl <jniederm@redhat.com>

--- src/main/java/types/OperatingSystemInfo.java
+++ src/main/java/types/OperatingSystemInfo.java
@@ -18,8 +18,30 @@ package types;
 
 import org.ovirt.api.metamodel.annotations.Type;
 
+/**
+ * Represents a guest operating system.
+ *
+ * @author Jakub Niedermertl <jniederm@redhat.com>
+ * @date 24 Apr 2017
+ * @state added
+ */
 @Type
 public interface OperatingSystemInfo extends Identified {
+    /**
+     * Large icon of the guest operating system. Maximum dimensions width: 150px, height: 120px.
+     *
+     * @author Jakub Niedermertl <jniederm@redhat.com>
+     * @date 24 Apr 2017
+     * @state added
+     */
     Icon largeIcon();
+
+    /**
+     * Small icon of the guest operating system. Maximum dimensions width: 43px, height: 43px.
+     *
+     * @author Jakub Niedermertl <jniederm@redhat.com>
+     * @date 24 Apr 2017
+     * @state added
+     */
     Icon smallIcon();
 }
