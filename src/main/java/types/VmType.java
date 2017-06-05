f075f9bde9b48a1f47e0a6c644c29152f4c5c2f7
Sharon Gratch <sgratch@redhat.com>
2017-06-05 18:37:50 +0300

    Add a new VM Type value named HIGH_PERFORMANCE.
    This is required for handling VMs optimized for high performance.
    Please see the feature page
    https://www.ovirt.org/develop/release-management/features/virt/high-performance-vm/
    for details.
    
    Change-Id: Ia10da0d427e38c5cbfdb722ec1d77827fe3ff4fc
    Signed-off-by: Sharon Gratch <sgratch@redhat.com>

--- src/main/java/types/VmType.java
+++ src/main/java/types/VmType.java
@@ -21,28 +21,73 @@ import org.ovirt.api.metamodel.annotations.Type;
  * Type representing what the virtual machine is optimized for.
  *
  * @author Arik Hadas <ahadas@redhat.com>
- * @date 12 Dec 2016
- * @status added
+ * @author Tahlia Richardson <trichard@redhat.com>
+ * @date 15 Sep 2017
+ * @status updated_by_docs
  */
 @Type
 public enum VmType {
     /**
      * The virtual machine is intended to be used as a desktop.
-     * Currently, its implication is that a sound device will be automatically added to the virtual machine.
+     *
+     * Currently, its implication is that a sound device will automatically be added to the virtual machine.
      *
      * @author Arik Hadas <ahadas@redhat.com>
-     * @date 12 Dec 2016
-     * @status added
+     * @author Tahlia Richardson <trichard@redhat.com>
+     * @date 15 Sep 2017
+     * @status updated_by_docs
      */
     DESKTOP,
 
      /**
      * The virtual machine is intended to be used as a server.
-     * Currently, its implication is that a sound device will not be automatically added to the virtual machine.
+     *
+     * Currently, its implication is that a sound device will not automatically be added to the virtual machine.
      *
      * @author Arik Hadas <ahadas@redhat.com>
-     * @date 12 Dec 2016
-     * @status added
+     * @author Tahlia Richardson <trichard@redhat.com>
+     * @date 15 Sep 2017
+     * @status updated_by_docs
      */
-    SERVER;
+    SERVER,
+
+    /**
+     * The virtual machine is intended to be used as a high performance virtual machine.
+     *
+     * Currently, its implication is that the virtual machine configuration will automatically be set for running
+     * with the highest possible performance, and with performance metrics as close to bare metal as possible.
+     *
+     * Some of the recommended configuration settings for the highest possible performance
+     * cannot be set automatically; manually setting them before running the virtual machine is recommended.
+     *
+     * The following configuration changes are set automatically:
+     *
+     * - Enable headless mode.
+     * - Enable serial console.
+     * - Enable pass-through host CPU.
+     * - Enable I/O threads.
+     * - Enable I/O threads pinning and set the pinning topology.
+     * - Enable the paravirtualized random number generator PCI (virtio-rng) device.
+     * - Disable all USB devices.
+     * - Disable the soundcard device.
+     * - Disable the smartcard device.
+     * - Disable the memory balloon device.
+     * - Disable the watchdog device.
+     * - Disable migration.
+     * - Disable high availability.
+     *
+     * The following recommended configuration changes have to be set manually by the user:
+     *
+     * - Enable CPU pinning topology.
+     * - Enable non-uniform memory access (NUMA) pinning topology.
+     * - Enable and set huge pages configuration.
+     * - Disable kernel same-page merging (KSM).
+     *
+     * @author Sharon Gratch <sgratch@redhat.com>
+     * @author Tahlia Richardson <trichard@redhat.com>
+     * @date 15 Sep 2017
+     * @status updated_by_docs
+     * @since 4.2
+     */
+    HIGH_PERFORMANCE;
 }
