8640313833f0f19416a5e98deee726ea4b06d0e3
Maor Lipchuk <mlipchuk@redhat.com>
2017-09-07 20:36:27 +0300

    Mark unregistered flag of add disk as deprecated
    The registration of unregistered disk has been moved to AttachedStorageDomainDisk.
    
    Change-Id: I4b3c5e59345289bdf1d3814a430e4e1c5e2eaffe
    Signed-off-by: Maor Lipchuk <mlipchuk@redhat.com>

--- src/main/java/services/StorageDomainDisksService.java
+++ src/main/java/services/StorageDomainDisksService.java
@@ -23,11 +23,12 @@ import org.ovirt.api.metamodel.annotations.Service;
 import types.Disk;
 
 /**
- * Manages the collection of disks available inside an specific storage domain.
+ * Manages the collection of disks available inside a specific storage domain.
  *
  * @author Juan Hernandez <juan.hernandez@redhat.com>
- * @date 4 Jan 2017
- * @status added
+ * @author Tahlia Richardson <trichard@redhat.com>
+ * @date 13 Sep 2017
+ * @status updated_by_docs
  */
 @Service
 @Area("Storage")
@@ -35,15 +36,16 @@ public interface StorageDomainDisksService {
     /**
      * Adds or registers a disk.
      *
-     * IMPORTANT: Since version 4.2 of the engine this operation is deprecated, and preserved only for backwards
+     * IMPORTANT: Since version 4.2 of the {engine-name} this operation is deprecated, and preserved only for backwards
      * compatibility. It will be removed in the future. To add a new disk use the <<services/disks/methods/add, add>>
      * operation of the service that manages the disks of the system. To register an unregistered disk use the
      * <<services/attached_storage_domain_disk/methods/register, register>> operation of the service that manages
      * that disk.
      *
      * @author Juan Hernandez <juan.hernandez@redhat.com>
-     * @date 14 Sep 2016
-     * @status added
+     * @author Tahlia Richardson <trichard@redhat.com>
+     * @date 13 Sep 2017
+     * @status updated_by_docs
      */
     @Deprecated
     interface Add {
@@ -51,15 +53,16 @@ public interface StorageDomainDisksService {
          * The disk to add or register.
          *
          * @author Juan Hernandez <juan.hernandez@redhat.com>
-         * @date 4 Jan 2017
-         * @status added
+         * @author Tahlia Richardson <trichard@redhat.com>
+         * @date 13 Sep 2017
+         * @status updated_by_docs
          */
         @In @Out Disk disk();
 
         /**
          * Indicates if a new disk should be added or if an existing unregistered disk should be registered. If the
          * value is `true` then the identifier of the disk to register needs to be provided. For example, to register
-         * the disk with id `456` send a request like this:
+         * the disk with ID `456` send a request like this:
          *
          * ....
          * POST /ovirt-engine/api/storagedomains/123/disks?unregistered=true
@@ -73,7 +76,7 @@ public interface StorageDomainDisksService {
          * ----
          *
          * If the value is `false` then a new disk will be created in the storage domain. In that case the
-         * `provisioned_size`, `format` and `name` attributes are mandatory. For example, to create a new
+         * `provisioned_size`, `format`, and `name` attributes are mandatory. For example, to create a new
          * _copy on write_ disk of 1 GiB, send a request like this:
          *
          * ....
@@ -93,38 +96,46 @@ public interface StorageDomainDisksService {
          *
          * The default value is `false`.
          *
+         * This parameter has been deprecated since version 4.2 of
+         * the {engine-name}.
+         *
          * @author Juan Hernandez <juan.hernandez@redhat.com>
-         * @date 4 Jan 2017
-         * @status added
+         * @author Tahlia Richardson <trichard@redhat.com>
+         * @date 13 Sep 2017
+         * @status updated_by_docs
+         * @Deprecated 4.2
          */
         @In Boolean unregistered();
     }
 
     /**
-     * Retrieve the list of disks that are available in the storage domain.
+     * Retrieves the list of disks that are available in the storage domain.
      *
-     * The order of the returned list of disks isn't guaranteed.
+     * The order of the returned list of disks is not guaranteed.
      *
      * @author Juan Hernandez <juan.hernandez@redhat.com>
-     * @date 4 Jan 2017
-     * @status added
+     * @author Tahlia Richardson <trichard@redhat.com>
+     * @date 13 Sep 2017
+     * @status updated_by_docs
      */
     interface List {
         /**
-         * List of retrieved disks.
+         * The list of retrieved disks.
          *
          * @author Juan Hernandez <juan.hernandez@redhat.com>
-         * @date 4 Jan 2017
-         * @status added
+         * @author Tahlia Richardson <trichard@redhat.com>
+         * @date 13 Sep 2017
+         * @status updated_by_docs
          */
         @Out Disk[] disks();
 
         /**
-         * Sets the maximum number of disks to return. If not specified all the disks are returned.
+         * Sets the maximum number of disks to return. If not specified, all the disks are returned.
          *
          * @author Juan Hernandez <juan.hernandez@redhat.com>
-         * @date 4 Jan 2017
-         * @status added
+         * @author Tahlia Richardson <trichard@redhat.com>
+         * @date 13 Sep 2017
+         * @status updated_by_docs
          */
         @In Integer max();
 
@@ -138,22 +149,24 @@ public interface StorageDomainDisksService {
          * ....
          *
          * The default value of the unregistered flag is `false`.
-         * The request only apply to storage domains that are attached.
+         * The request only applies to storage domains that are attached.
          *
          * @author Maor Lipchuk <mlipchuk@redhat.com>
-         * @date 8 Mar 2017
-         * @status added
+         * @author Tahlia Richardson <trichard@redhat.com>
+         * @date 13 Sep 2017
+         * @status updated_by_docs
          * @since 4.1.1
          */
         @In Boolean unregistered();
     }
 
     /**
-     * Reference to the service that manages a specific disk.
+     * A reference to the service that manages a specific disk.
      *
      * @author Juan Hernandez <juan.hernandez@redhat.com>
-     * @date 4 Jan 2017
-     * @status added
+     * @author Tahlia Richardson <trichard@redhat.com>
+     * @date 13 Sep 2017
+     * @status updated_by_docs
      */
     @Service StorageDomainDiskService disk(String id);
 }
