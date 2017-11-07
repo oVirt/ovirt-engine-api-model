c500e8b2c88f5a6f822f920a9cc8add1ace5a3d5
Idan Shaby <ishaby@redhat.com>
2017-11-07 10:23:18 +0200

    Add exception for the type of ImageTransfer::transferred
    The type of ImageTransfer::transferred is Integer, but since it is not
    big enough to hold (in the worst case) a file's size in bytes, the type
    should be changed to Long.
    
    Change-Id: I120c5bce8ed4cc44b465f385f3d12dccbda6c5cf
    Signed-off-by: Idan Shaby <ishaby@redhat.com>

--- tool/src/main/java/org/ovirt/api/metamodel/tool/SchemaGenerator.java
+++ tool/src/main/java/org/ovirt/api/metamodel/tool/SchemaGenerator.java
@@ -117,6 +117,7 @@ public class SchemaGenerator {
         addMemberSchemaTypeNameException("host-nic", "speed", "xs:long");
         addMemberSchemaTypeNameException("image", "size", "xs:long");
         addMemberSchemaTypeNameException("image-transfer", "size", "xs:long");
+        addMemberSchemaTypeNameException("image-transfer", "transferred", "xs:long");
         addMemberSchemaTypeNameException("logical-unit", "discard-max-size", "xs:long");
         addMemberSchemaTypeNameException("logical-unit", "size", "xs:long");
         addMemberSchemaTypeNameException("memory-policy", "guaranteed", "xs:long");
