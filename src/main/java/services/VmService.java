8c43d9178a058b8b2d0b09746ee3299bcc32187c
Eyal Shenitzky <eshenitz@redhat.com>
2018-01-24 09:39:15 +0200

    restapi: fix documentation for lease param in custom preview
    Change-Id: Ie4acbd5216c1c5d9a4d55fd0c613d15d68532053
    Signed-off-by: Eyal Shenitzky <eshenitz@redhat.com>
    (cherry picked from commit a30d348f0624f5fc995daea380ef5302c94c5640)

--- src/main/java/services/VmService.java
+++ src/main/java/services/VmService.java
@@ -609,8 +609,8 @@ public interface VmService extends MeasurableService {
          * If lease parameter is not passed, then the previewed snapshot lease storage domain will be used.
          * If lease parameter is passed with empty storage domain parameter, then no lease will be used
          * for the snapshot preview.
-         * If lease parameter is passed with storage domain parameter then the storage domain ID can be the
-         * active snapshot lease storage domain ID or, the previewed snapshot lease storage domain ID only.
+         * If lease parameter is passed with storage domain parameter then the storage domain ID can be
+         * only one of the leases domain IDs that belongs to one of the virtual machine snapshots.
          * This is an optional parameter, set by default to `null`
          *
          * @author Eyal Shenitzky <eshenitz@redhat.com>
