e3b29c4afcef5dde4761ed980deb3bffa7ebdc60
Jasper Berton <jasper.berton@team.blue>
2025-02-07 16:27:20 +0100

    legal: Fixed copyright files
    Altered headers on all files to reference the ovirt authors team and not Redhat
    
    Signed-off-by: Jasper Berton <jasper.berton@team.blue>

--- src/main/java/types/RegistrationConfiguration.java
+++ src/main/java/types/RegistrationConfiguration.java
@@ -1,5 +1,7 @@
 /*
-Copyright (c) 2017 Red Hat, Inc.
+The oVirt Project - oVirt Engine Api Model
+
+Copyright oVirt Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -9,9 +11,12 @@ You may obtain a copy of the License at
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRAN/TIES OR CONDITIONS OF ANY KIND, either express or implied.
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+
+A copy of the Apache License, Version 2.0 is included with the program
+in the file ASL2.
 */
 
 package types;
