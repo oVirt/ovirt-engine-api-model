f2ba9721aa626b0182a5a6c5d014ee364cfb479d
Jasper Berton <jasper.berton@team.blue>
2025-02-07 16:27:20 +0100

    legal: Fixed copyright files
    Altered headers on all files to reference the ovirt authors team and not Redhat
    
    Signed-off-by: Jasper Berton <jasper.berton@team.blue>

--- src/main/java/types/StorageDomainLease.java
+++ src/main/java/types/StorageDomainLease.java
@@ -1,5 +1,7 @@
 /*
-Copyright (c) 2015-2017 Red Hat, Inc.
+The oVirt Project - oVirt Engine Api Model
+
+Copyright oVirt Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,6 +14,9 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+
+A copy of the Apache License, Version 2.0 is included with the program
+in the file ASL2.
 */
 
 package types;
